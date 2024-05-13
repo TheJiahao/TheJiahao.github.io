71ca0a4c384f94f133ca4f379d939cd32f8eb1bf
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-13 17:45:17 +0300

    feat: Add content collection for posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,4 +13,14 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const postCollection = defineCollection({
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        date: z.string(),
+        tags: z.array(z.string()),
+        image: z.string().optional(),
+    }),
+});
+
+export const collections = { blog, posts: postCollection };
