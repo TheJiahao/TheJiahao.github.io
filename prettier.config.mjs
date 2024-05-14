2499ca7f346c6f67c3ed0132b617dc905b1d4f60
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-14 17:01:09 +0300

    ci: Add prettier-plugin-astro

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,4 +2,13 @@
 
 export default {
     tabWidth: 4,
+    plugins: ["prettier-plugin-astro"],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
 };
