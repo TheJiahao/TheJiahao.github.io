553d03bcffadd61d767c86879bf22aceacc58847
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-14 12:59:21 +0300

    fix: Astro UnoCSS integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://example.com",
-    integrations: [mdx(), sitemap(), react()],
+    integrations: [
+        mdx(),
+        sitemap(),
+        react(),
+        UnoCSS({
+            injectReset: true, // or a path to the reset file
+        }),
+    ],
 });
