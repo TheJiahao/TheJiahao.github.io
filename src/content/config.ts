32aa19c867b05fd2c801d45a4b40b4a7f5d66b4d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-14 21:06:46 +0300

    feat(#11): Add cover image to frontmatter

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,14 @@ import { defineCollection, z } from "astro:content";
 
 const postCollection = defineCollection({
     type: "content",
-    schema: z.object({
-        title: z.string(),
-        date: z.date(),
-        tags: z.array(z.string()),
-        description: z.string().optional(),
-        image: z.string().optional(),
-    }),
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            date: z.date(),
+            tags: z.array(z.string()),
+            description: z.string().optional(),
+            image: image(),
+        }),
 });
 
 export const collections = { posts: postCollection };
