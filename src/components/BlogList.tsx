2053ae96ff89ce35c54b1f63f079689a8cf51608
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-14 15:04:43 +0300

    feat(#10): Add placeholder BlogList

---
+++ src/components/BlogList.tsx
@@ -0,0 +1,11 @@
+import "../styles/card.css"
+
+const BlogList = () => {
+    return (
+        <div className="card">
+            <h2>Blog List</h2>
+        </div>
+    );
+}
+
+export default BlogList;
