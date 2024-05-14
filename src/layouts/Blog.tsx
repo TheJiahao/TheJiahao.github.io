f8fa29d541a70ad00c5de80103ad47f15acc1d54
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-14 21:59:31 +0300

    feat(#11): Show blog cover image and date

--- src/layouts/Blog.tsx
+++ src/layouts/Blog.tsx
@@ -8,7 +8,7 @@ interface BlogProps extends postData {
     children: ReactNode;
 }
 
-const BlogPost = ({ title, description, date, children }: BlogProps) => {
+const BlogPost = ({ title, description, date, image, children }: BlogProps) => {
     return (
         <article
             className="card prose"
@@ -16,7 +16,28 @@ const BlogPost = ({ title, description, date, children }: BlogProps) => {
             un-max-w="prose"
             un-mx="auto"
         >
-            {children}
+            <header>
+                {image && (
+                    <img
+                        src={image.src}
+                        alt={`Cover image of "${title}"`}
+                        className="[&[src$='svg']]:object-fill"
+                        un-w="full"
+                        un-max-w="full"
+                        un-max-h="30vh"
+                        un-object="cover"
+                        un-overflow="hidden"
+                    />
+                )}
+                <div un-px="8" un-py="2">
+                    <h2>{title}</h2>
+                    <h3 un-text="slate">{description}</h3>
+                    <time>{date.toISOString().split("T")[0]}</time>
+                </div>
+            </header>
+            <section un-px="8" un-py="2">
+                {children}
+            </section>
         </article>
     );
 };
