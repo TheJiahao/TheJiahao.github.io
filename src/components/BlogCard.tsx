fbcb79680d0392a0234903e56d055805095d4516
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-15 21:31:58 +0300

    feat(#12): Make BlogCard clickable

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -2,39 +2,48 @@ import type { CollectionEntry } from "astro:content";
 import "../styles/card.css";
 import formatDate from "../utils/formatDate";
 
+type BlogProps = CollectionEntry<"posts">["data"] & { slug: string };
+
 const width = 200;
 const height = width / 1.618;
 const detailsHeight = (1 / 2.618) * height;
-const imageHeight = height - detailsHeight;
 
 const BlogCard = ({
     title,
     description,
     date,
+    slug,
     image = {
         src: "/src/assets/images/cover_placeholder.svg",
         width,
         height,
         format: "svg",
     },
-}: CollectionEntry<"posts">["data"]) => {
+}: BlogProps) => {
     return (
-        <div className="card" un-w="sm" un-aspect-ratio="1.618">
+        <a className="card flex" href={`/posts/${slug}`} un-w="90ch">
             <img
                 src={image.src}
                 alt={`Cover of "${title}"`}
                 className="[&[src$='svg']]:object-fill"
                 un-object="cover"
-                un-h={(1.618 / 2.618) * height}
                 un-w="full"
+                un-max-h="30vh"
             />
-            <div un-h={(1 / 2.618) * height}>
+
+            <div
+                className="prose block"
+                un-h={detailsHeight}
+                un-p="8"
+                un-max-w="full"
+            >
                 <h2>{title}</h2>
                 <p>{description}</p>
                 <time>{formatDate(date)}</time>
             </div>
-        </div>
+        </a>
     );
 };
 
+export type { BlogProps };
 export default BlogCard;
