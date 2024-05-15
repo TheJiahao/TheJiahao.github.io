09a3ef5e0347470d6e153a2cae936e132fdd5ed9
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-15 21:30:04 +0300

    chore(#12): Add gap between BlogCard

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -4,7 +4,7 @@ import BlogCard from "./BlogCard";
 
 const BlogList = ({ blogs }: { blogs: CollectionEntry<"posts">["data"][] }) => {
     return (
-        <div className="flex" un-flex="col">
+        <div className="flex" un-flex="col" un-gap="10">
             {blogs.map((blog) => (
                 <BlogCard key={blog.title} {...blog} />
             ))}
