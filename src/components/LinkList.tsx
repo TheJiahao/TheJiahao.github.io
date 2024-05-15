c7c5c33fde9b2d4a07fae22ae841d4e8551c7886
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-15 18:06:11 +0300

    refactor(#11): Make link list mandatory

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -2,17 +2,15 @@ import type { LinkProps } from "./Link";
 import Link from "./Link";
 import "../styles/card.css";
 
-const LinkList = ({ links }: { links?: LinkProps[] }) => {
-    if (!links) {
-        return null;
-    }
-
+const LinkList = ({ links }: { links: LinkProps[] }) => {
     return (
-        <div className="card" un-w="full" un-mx="auto" un-divide="y">
-            {links.map((link) => (
-                <Link key={link.url} {...link} />
-            ))}
-        </div>
+        links && (
+            <div className="card" un-w="full" un-mx="auto" un-divide="y">
+                {links.map((link) => (
+                    <Link key={link.url} {...link} />
+                ))}
+            </div>
+        )
     );
 };
 
