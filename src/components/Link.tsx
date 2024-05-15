f3a29b207aa053086d4ef29064ae0ecd9247e9c5
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-15 17:44:32 +0300

    feat(#11): Add initial link listing

---
+++ src/components/Link.tsx
@@ -0,0 +1,27 @@
+interface LinkProps {
+    title: string;
+    description?: string;
+    url: string;
+    image?: string;
+}
+
+const Link = ({ title, description, url, image }: LinkProps) => {
+    return (
+        <a href={url} className="flex" un-gap="4" un-px="8" un-py="2">
+            <div un-flex="auto">
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+            <img
+                src={image}
+                alt={`Cover image of "${title}"`}
+                width="50vh"
+                height="50vh"
+                un-flex="none"
+            />
+        </a>
+    );
+};
+
+export type { LinkProps };
+export default Link;
