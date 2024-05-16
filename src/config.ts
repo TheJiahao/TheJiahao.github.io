f88d56212316e762baf8a0dee5fe753a28d9913d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-16 12:14:18 +0300

    refactor: Move blog image placeholder to config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,3 +10,13 @@ export const NAVIGATION_LINKS = [
     { href: "/posts", text: "归档", icon: "i-fluent-emoji-flat-file-cabinet" },
     { href: "/about", text: "关于", icon: "i-fluent-emoji-flat-star" },
 ];
+
+export const BLOG_IMAGE_WIDTH = 200;
+export const BLOG_IMAGE_HEIGHT = BLOG_IMAGE_WIDTH / 1.618;
+
+export const BLOG_IMAGE_PLACEHOLDER = Object.freeze({
+    src: "/src/assets/images/cover_placeholder.svg",
+    width: BLOG_IMAGE_WIDTH,
+    height: BLOG_IMAGE_HEIGHT,
+    format: "svg",
+});
