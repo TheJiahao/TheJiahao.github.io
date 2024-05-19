92cc5e3120b02c3b1e68537c8aeb5b2669533cf5
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-19 08:35:55 +0300

    refactor:Rename Link to LinkCard

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -1,12 +1,12 @@
-import type { LinkProps } from "./Link";
-import Link from "./Link";
+import type { LinkProps } from "./LinkCard";
+import LinkCard from "./LinkCard";
 
 const LinkList = ({ links }: { links: LinkProps[] }) => {
     return (
         links && (
             <div className="card" un-w="full" un-mx="auto" un-divide="y">
                 {links.map((link) => (
-                    <Link key={link.url} {...link} />
+                    <LinkCard key={link.url} {...link} />
                 ))}
             </div>
         )
