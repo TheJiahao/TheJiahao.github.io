9b5df612c1469f3785d1ded5a85b47a8a4b711a6
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-19 09:12:38 +0300

    chore: Add attributify types for Astro

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -3,3 +3,9 @@ import type { AttributifyAttributes } from "unocss/preset-attributify";
 declare module "react" {
     interface HTMLAttributes<>extends AttributifyAttributes {}
 }
+
+declare global {
+    namespace astroHTML.JSX {
+        interface HTMLAttributes extends AttributifyAttributes {}
+    }
+}
