5a8aa8ed5185bcbb698d8a2e1fbb05a28e31f6be
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-19 11:58:46 +0300

    feat(UnoCSS): Add custom attributify attributes

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,11 +1,27 @@
-import type { AttributifyAttributes } from "unocss/preset-attributify";
+import type {
+    AttributifyAttributes,
+    PseudoPrefix,
+} from "unocss/preset-attributify";
+
+type customAttributes = "prose" | "card";
+type customAttributifyNames<Prefix extends string = ""> =
+    | `${Prefix}${customAttributes}`
+    | `${Prefix}${PseudoPrefix}:${customAttributes}`;
+
+type customAttribifyAttributes = Partial<
+    Record<customAttributifyNames, string | boolean>
+>;
 
 declare module "react" {
-    interface HTMLAttributes extends AttributifyAttributes {}
+    interface HTMLAttributes
+        extends AttributifyAttributes,
+            customAttribifyAttributes {}
 }
 
 declare global {
     namespace astroHTML.JSX {
-        interface HTMLAttributes extends AttributifyAttributes {}
+        interface HTMLAttributes
+            extends AttributifyAttributes,
+                customAttribifyAttributes {}
     }
 }
