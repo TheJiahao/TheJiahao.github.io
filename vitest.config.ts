d73e2aa591e07fb4d84392250c9b0877aee22139
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-19 16:53:53 +0300

    ci(#10): Configure coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,10 @@ export default getViteConfig({
     test: {
         environment: "jsdom",
         setupFiles: ["/src/tests/setup.ts"],
+        coverage: {
+            reporter: ["json", "html"],
+            include: ["src"],
+            exclude: ["src/tests/**", "**/*.d.ts"],
+        },
     },
 });
