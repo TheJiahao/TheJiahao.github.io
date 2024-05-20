3f2f31c4cd1e167acd90fc44da5453b96a556462
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-20 20:57:06 +0300

    test(e2e): Remove automatic retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,6 @@ export default defineConfig({
     testDir: "src/tests/e2e",
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
     reporter: process.env.CI ? [["github"], ["dot"]] : [["html"], ["list"]],
     timeout: 10000,
     expect: {
