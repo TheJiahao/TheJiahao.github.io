6d6e5e1a11d46d836bdb0ee4a0b6fd4a6ee06665
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-20 18:04:31 +0300

    feat(accessibility): Add accessible roles and names to BlogList

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -2,11 +2,13 @@ import BlogCard, { type BlogProps } from "./BlogCard";
 
 const BlogList = ({ blogs }: { blogs: BlogProps[] }) => {
     return (
-        <div flex="~ col" gap="10">
+        <ul aria-label="List of blogs" flex="~ col" gap="10">
             {blogs.map((blog) => (
-                <BlogCard key={blog.title} {...blog} />
+                <li key={blog.title} aria-label="Blog">
+                    <BlogCard {...blog} />
+                </li>
             ))}
-        </div>
+        </ul>
     );
 };
 
