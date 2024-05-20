c6f21b413cb02325fe8acf637165abb5f389efac
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-20 14:19:17 +0300

    test(#10): Configure Playwright reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
-    reporter: "html",
+    reporter: process.env.CI ? "github" : [["html"], ["list"]],
     use: {
         baseURL: BASE_URL,
         trace: "on-first-retry",
