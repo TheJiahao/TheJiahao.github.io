2a367e4be48126e45b41b7860f4fb9506e2f8570
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-20 13:50:48 +0300

    test(#10): Exclude e2e tests for Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { getViteConfig } from "astro/config";
 
 export default getViteConfig({
     test: {
+        include: ["src/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+        exclude: ["src/tests/e2e/**"],
         environment: "jsdom",
         setupFiles: ["src/tests/setup.ts"],
         reporters: "default",
