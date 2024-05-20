8158fdd9fe9097a9cdb1d1c5323f584d3b057370
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-20 17:58:20 +0300

    test(#3): Add e2e tests
    Closes #3

--- src/tests/e2e/homepage.test.tsx
+++ src/tests/e2e/homepage.test.tsx
@@ -1,4 +1,4 @@
-import test, { expect } from "@playwright/test";
+import test, { expect, type Locator } from "@playwright/test";
 
 test.describe("Homepage", () => {
     test("can be accessed", async ({ page }) => {
@@ -9,7 +9,48 @@ test.describe("Homepage", () => {
         await page.goto("/");
     });
 
-    test("has a title", async ({ page }) => {
+    test("has correct title", async ({ page }) => {
         await expect(page).toHaveTitle("Jiahao 的博客");
+    });
+
+    test.describe("navigation bar", () => {
+        let navigationBar: Locator;
+
+        test.beforeEach(async ({ page }) => {
+            navigationBar = page.getByRole("navigation");
+        });
+
+        test("exists", async () => {
+            expect(navigationBar).toBeDefined();
+
+            await expect(navigationBar).toHaveCount(1);
+        });
+
+        test("has link to homepage", async () => {
+            const link = navigationBar
+                .getByRole("link")
+                .filter({ hasText: "首页" });
+
+            expect(link).toBeDefined();
+            await expect(link).toHaveAttribute("href", "/");
+        });
+
+        test("has link to posts page", async () => {
+            const link = navigationBar
+                .getByRole("link")
+                .filter({ hasText: "归档" });
+
+            expect(link).toBeDefined();
+            await expect(link).toHaveAttribute("href", "/posts");
+        });
+
+        test("has link to about page", async () => {
+            const link = navigationBar
+                .getByRole("link")
+                .filter({ hasText: "关于" });
+
+            expect(link).toBeDefined();
+            await expect(link).toHaveAttribute("href", "/about");
+        });
     });
 });
