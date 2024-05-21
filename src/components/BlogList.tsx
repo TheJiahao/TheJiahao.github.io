424a9c09f954db55b207e659c1861b89c5a6ea77
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-21 20:34:32 +0300

    refactor: Move styles from `BlogCard` to `BlogList` for scalability

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -2,7 +2,7 @@ import BlogCard, { type BlogProps } from "./BlogCard";
 
 const BlogList = ({ blogs }: { blogs: BlogProps[] }) => {
     return (
-        <ul aria-label="List of blogs" flex="~ col" gap="10">
+        <ul aria-label="List of blogs" flex="~ col" max-w-90ch mx-auto gap="10">
             {blogs.map((blog) => (
                 <li key={blog.title} aria-label="Blog">
                     <BlogCard {...blog} />
