bbf60d9bef6e9f9fa677da0df633146eecc6898c
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-21 14:53:07 +0300

    test(#10): Add tests for `BlogList`

---
+++ src/tests/components/BlogList.test.tsx
@@ -0,0 +1,44 @@
+import { beforeEach, describe, expect, test } from "vitest";
+import type { BlogProps } from "../../components/BlogCard";
+import { render, screen, within } from "@testing-library/react";
+import BlogList from "../../components/BlogList";
+
+describe("<BlogList/>", () => {
+    const blogs: BlogProps[] = [
+        {
+            title: "How to write unit tests for Astro components?",
+            description:
+                "This blog post will teach you how to write unit tests for Astro components.",
+            date: new Date("2020-12-11"),
+            slug: "astro-unit-tests",
+        },
+        {
+            title: "如何写出一篇好文章？",
+            description: "本文介绍了如何写出一篇好文章。",
+            date: new Date("2019-01-05"),
+            slug: "how-to-write-good-articles",
+        },
+    ];
+
+    let blogList: HTMLElement;
+
+    beforeEach(() => {
+        render(<BlogList blogs={blogs} />);
+        blogList = screen.getByRole("list", { name: "List of blogs" });
+    });
+
+    test("is rendered", () => {
+        expect(blogList).toBeInTheDocument();
+    });
+
+    test("contains given blogs", () => {
+        expect(within(blogList).getAllByRole("listitem")).toHaveLength(2);
+    });
+
+    test("blogs have correct titles", () => {
+        const blogTitles = screen.getAllByRole("heading");
+
+        expect(blogTitles[0]).toHaveTextContent(blogs[0].title);
+        expect(blogTitles[1]).toHaveTextContent(blogs[1].title);
+    });
+});
