87399179c5a21cec36e4c689c7af347d366b498e
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-21 15:46:00 +0300

    test(#10): Add tests for `LinkList`

---
+++ src/tests/components/LinkList.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen, within } from "@testing-library/react";
+import { beforeEach, describe, expect, test } from "vitest";
+import type { LinkProps } from "../../components/LinkCard";
+import LinkList from "../../components/LinkList";
+
+describe("<LinkList/>", () => {
+    const links: LinkProps[] = [
+        {
+            title: "How to write unit tests for Astro components?",
+            description:
+                "This blog post will teach you how to write unit tests for Astro components.",
+            url: "https://www.blog.com/astro-unit-tests",
+        },
+        {
+            title: "如何写出一篇好文章？",
+            description: "本文介绍了如何写出一篇好文章。",
+            url: "https://another.blog.com/how-to-write-good-articles",
+        },
+    ];
+
+    let linkList: HTMLElement;
+
+    beforeEach(() => {
+        render(<LinkList links={links} />);
+        linkList = screen.getByRole("list", { name: "External links" });
+    });
+
+    test("is rendered", () => {
+        expect(linkList).toBeInTheDocument();
+    });
+
+    test("contains given links", () => {
+        expect(within(linkList).getAllByRole("listitem")).toHaveLength(2);
+    });
+
+    test("links have correct titles", () => {
+        const titles = screen.getAllByRole("heading");
+
+        expect(titles[0]).toHaveTextContent(links[0].title);
+        expect(titles[1]).toHaveTextContent(links[1].title);
+    });
+});
