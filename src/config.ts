1b0b0f61a16e65b7cc9ef3c233f3003e6fbec1a6
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-23 15:05:21 +0300

    feat(#17): Add site footer

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,13 @@ export const SITE_TITLE = "Jiahao 的博客";
 export const SITE_DESCRIPTION =
     "个人博客，内容可能涵盖工具配置、技术、数学等，不定时更新。";
 export const SITE_AVATAR = await getImage("/src/assets/images/avatar.svg");
+export const SITE_OWNER = "Jiahao Li";
+export const SITE_LICENSE = {
+    name: "CC BY-NC-SA 4.0",
+    url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+export const SITE_START_YEAR = 2020;
+export const SITE_SOURCE = "https://github.com/TheJiahao/TheJiahao.github.io";
 
 export const NAVIGATION_LINKS = [
     { url: "/", text: "首页", icon: "i-fluent-emoji-flat-house" },
