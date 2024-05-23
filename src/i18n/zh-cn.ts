95526231139437158d822bacf8a1cf1620299194
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-23 19:42:27 +0300

    refactor(#21): Extend zh-CN translation from en

--- src/i18n/zh-cn.ts
+++ src/i18n/zh-cn.ts
@@ -1,11 +1,14 @@
+import defaultLanguage from "./default";
+
 const zh_cn = {
+    ...defaultLanguage,
     name: "简体中文",
     siteTitle: "Jiahao 的博客",
     siteDescription:
         "个人博客，内容可能涵盖工具配置、技术、数学等，不定时更新。",
     homePage: "首页",
     archive: "归档",
-    about: "关于"
+    about: "关于",
 };
 
 export default zh_cn;
