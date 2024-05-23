92ae2b346e55081f57a608332bddd699fcdfe183
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-23 19:37:33 +0300

    feat(#21): Add zh-CN translation

---
+++ src/i18n/zh-cn.ts
@@ -0,0 +1,11 @@
+const zh_cn = {
+    name: "简体中文",
+    siteTitle: "Jiahao 的博客",
+    siteDescription:
+        "个人博客，内容可能涵盖工具配置、技术、数学等，不定时更新。",
+    homePage: "首页",
+    archive: "归档",
+    about: "关于"
+};
+
+export default zh_cn;
