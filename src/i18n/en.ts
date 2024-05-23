199eb8cf4197e3295f4eeabe88193f3b4632c2b0
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-23 19:39:38 +0300

    feat(#21): Add en translation

---
+++ src/i18n/en.ts
@@ -0,0 +1,11 @@
+const en = {
+    name: "English",
+    siteTitle: "Jiahao's blog",
+    siteDescription:
+        "Personal blog, might contain content about configuring tools, technology, math, etc. Updated irregularly.",
+    homePage: "Home",
+    archive: "Archive",
+    about: "About",
+};
+
+export default en;
