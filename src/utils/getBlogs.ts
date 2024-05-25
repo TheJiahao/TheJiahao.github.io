510e556f0f8cdf50c7e2be5e6d432c95e293aa05
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-25 19:33:32 +0300

    refactor: Add return type to `getBlogs`

--- src/utils/getBlogs.ts
+++ src/utils/getBlogs.ts
@@ -1,6 +1,6 @@
-import { getCollection } from "astro:content";
+import { getCollection, type CollectionEntry } from "astro:content";
 
-const getBlogs = async () => {
+const getBlogs = async (): Promise<CollectionEntry<"posts">[]> => {
     return await getCollection(
         "posts",
         ({ data }) => import.meta.env.MODE === "development" || !data.draft,
