11bcc93d144be59ff336c04b04ef85a167547955
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-25 20:30:27 +0300

    fix: Blog links in homepage

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -2,18 +2,18 @@ import type { CollectionEntry } from "astro:content";
 import { BLOG_IMAGE_PLACEHOLDER } from "../config";
 import formatDate from "../utils/formatDate";
 
-type BlogProps = CollectionEntry<"posts">["data"] & { slug: string };
+type BlogProps = CollectionEntry<"posts">["data"] & { url: string };
 
 const BlogCard = ({
     title,
     description,
     date,
-    slug,
+    url,
     image = BLOG_IMAGE_PLACEHOLDER,
 }: BlogProps) => {
     return (
         <article card>
-            <a href={`/posts/${slug}`} rel="bookmark">
+            <a href={url} rel="bookmark">
                 <img
                     src={image.src}
                     alt={`Cover of "${title}"`}
