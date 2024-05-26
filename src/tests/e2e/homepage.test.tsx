886b0f32a521d6886ea1a911de7dad8844bacf7f
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-26 17:42:45 +0300

    refactor(e2e): Move test of blog list to own file

--- src/tests/e2e/homepage.test.tsx
+++ src/tests/e2e/homepage.test.tsx
@@ -1,4 +1,4 @@
-import test, { expect, type Locator } from "@playwright/test";
+import test, { expect } from "@playwright/test";
 
 test.describe("Homepage", () => {
     test("can be accessed", async ({ page }) => {
@@ -11,30 +11,5 @@ test.describe("Homepage", () => {
 
     test("has correct title", async ({ page }) => {
         await expect(page).toHaveTitle("Jiahao 的博客");
-    });
-
-    test.describe("blog list", () => {
-        let blogList: Locator;
-
-        test.beforeEach(({ page }) => {
-            blogList = page.getByRole("list", { name: "List of blogs" });
-        });
-
-        test("exists", () => {
-            expect(blogList).toBeDefined();
-        });
-
-        test("has at least one blog", async () => {
-            const blogs = await blogList.getByRole("listitem").count();
-            expect(blogs).toBeGreaterThan(0);
-        });
-
-        test("clicking a blog leads to blog post", async ({ page }) => {
-            const firstBlog = blogList.getByRole("listitem").first();
-
-            await firstBlog.click();
-
-            await expect(page).toHaveURL(/\/posts\//);
-        });
     });
 });
---
+++ src/tests/e2e/navigate_to_blog.test.tsx
@@ -0,0 +1,27 @@
+import test, { expect, type Locator } from "@playwright/test";
+
+test.describe("blog list", () => {
+    let blogList: Locator;
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/");
+        blogList = page.getByRole("list", { name: "List of blogs" });
+    });
+
+    test("exists", () => {
+        expect(blogList).toBeDefined();
+    });
+
+    test("has at least one blog", async () => {
+        const blogs = await blogList.getByRole("listitem").count();
+        expect(blogs).toBeGreaterThan(0);
+    });
+
+    test("clicking a blog leads to blog post", async ({ page }) => {
+        const firstBlog = blogList.getByRole("listitem").first();
+
+        await firstBlog.click();
+
+        await expect(page).toHaveURL(/\/posts\//);
+    });
+});
