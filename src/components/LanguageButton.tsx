9408713b079f4c246a2451f1e6d6313ca257a85c
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-26 15:56:30 +0300

    refactor(#21): Inject languages to `LanguageButton`

--- src/components/LanguageButton.tsx
+++ src/components/LanguageButton.tsx
@@ -1,13 +1,25 @@
 import { getRelativeLocaleUrl } from "astro:i18n";
 import type { ChangeEventHandler } from "react";
-import { getTranslation, languages } from "../utils/translation";
+import {
+    languages as DEFAULT_LANGUAGES,
+    getTranslation,
+} from "../utils/translation";
+import type { Language } from "../interfaces/Language";
+
+interface LanguageButtonProps {
+    lang: string;
+    languages?: Pick<Language, "name" | "code">[];
+}
 
 const handleNavigation: ChangeEventHandler<HTMLSelectElement> = (event) => {
     const language = event.target.value;
     window.location.href = getRelativeLocaleUrl(language, "/");
 };
 
-const LanguageButton = ({ lang }: { lang: string }) => (
+const LanguageButton = ({
+    lang,
+    languages = DEFAULT_LANGUAGES,
+}: LanguageButtonProps) => (
     <div inline-flex items-center gap-2>
         <span className="i-fluent-emoji-flat-globe-with-meridians" />
         <select
