686011cc73cad196ad3c2a911a102838adf1ac7b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-26 15:48:37 +0300

    refactor(toolbar): Make lang mandatory

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,7 +1,6 @@
-import { DEFAULT_LOCALE } from "../config";
 import LanguageButton from "./LanguageButton";
 
-const ToolBar = ({ lang = DEFAULT_LOCALE }: { lang?: string }) => {
+const ToolBar = ({ lang }: { lang: string }) => {
     return (
         <aside card mx-auto p-2>
             <LanguageButton lang={lang} />
