7c119f79c38c5ab94e5537e9916745651a17fd19
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-26 11:50:53 +0300

    refactor: Move `ToolBar` to top

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,5 +1,11 @@
+import LanguageButton from "./LanguageButton";
+
 const ToolBar = () => {
-    return <aside card></aside>;
+    return (
+        <aside card mx-auto p-2>
+            <LanguageButton />
+        </aside>
+    );
 };
 
 export default ToolBar;
