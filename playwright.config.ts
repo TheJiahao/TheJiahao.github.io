c33aff3143967767dae8f044e5071ddfbea8ced2
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-27 08:23:33 +0300

    ci(e2e): Disable retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,6 @@ export default defineConfig({
     fullyParallel: true,
     maxFailures: process.env.CI ? 3 : undefined,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? "100%" : undefined,
     reporter: process.env.CI ? [["github"], ["dot"]] : [["html"], ["list"]],
     timeout: 10000,
