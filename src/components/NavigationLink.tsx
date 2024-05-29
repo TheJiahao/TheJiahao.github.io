8fb40dcbbff7bb87d87dfe9fef533da1437f6c48
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-29 12:57:41 +0300

    feat(#39): Allow passing extra classes to `NavigationLink`

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -1,11 +1,25 @@
+import classnames from "classnames";
+
 interface NavigationLinkProps {
     url: string;
     text: string;
     icon: string;
+    classNames?: string[];
 }
 
-const NavigationLink = ({ url, text, icon }: NavigationLinkProps) => (
-    <a href={url} inline-flex gap-10 leading-4>
+const NavigationLink = ({
+    url,
+    text,
+    icon,
+    classNames,
+}: NavigationLinkProps) => (
+    <a
+        href={url}
+        inline-flex
+        gap-10
+        leading-4
+        className={classnames(classNames)}
+    >
         <span className={icon} />
         <span>{text}</span>
     </a>
