adb5ab8a0d87292e4c61174c9bd550ff183eacea
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-30 21:35:29 +0300

    feat(#4): Add icon for updated date

---
+++ src/components/IconText.tsx
@@ -0,0 +1,14 @@
+import type { PropsWithChildren } from "react";
+
+interface IconTextProps extends PropsWithChildren {
+    icon: string;
+}
+
+const IconText = ({ icon, children }: IconTextProps) => (
+    <span inline-flex gap-1 leading-4>
+        <span className={icon}></span>
+        <span>{children}</span>
+    </span>
+);
+
+export default IconText;
