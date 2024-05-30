0079c175a74277145458af928c710b418e97339d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-05-30 14:46:58 +0300

    refactor: Use valueless attribute

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -2,7 +2,7 @@ import BlogCard, { type BlogCardProps } from "./BlogCard";
 
 const BlogList = ({ blogs }: { blogs: BlogCardProps[] }) => {
     return (
-        <ul aria-label="List of blogs" flex="~ col" gap="10">
+        <ul aria-label="List of blogs" flex="~ col" gap-10>
             {blogs.map((blog) => (
                 <li key={blog.title} aria-label="Blog">
                     <BlogCard {...blog} />
