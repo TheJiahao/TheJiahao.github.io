aa4fe8dd4b8e7806893cf1955f527c58262ede97
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-02 15:24:05 +0300

    refactor: Use absolute import in `getTranslation`

--- src/utils/getTranslation.ts
+++ src/utils/getTranslation.ts
@@ -1,5 +1,5 @@
 import { languages } from "localization";
-import type { Language } from "../interfaces/Language";
+import type { Language } from "interfaces/Language";
 
 const translations: Record<string, Language> = Object.fromEntries(
     languages.map((language) => [language.code, language]),
