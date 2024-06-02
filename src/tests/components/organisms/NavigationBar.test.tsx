9df13a24621872b59508be822a2a2c090e670429
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-02 20:16:20 +0300

    test(#38): Check header visibility

--- src/tests/components/organisms/NavigationBar.test.tsx
+++ src/tests/components/organisms/NavigationBar.test.tsx
@@ -1,3 +1,13 @@
-import { describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import NavigationBar from "components/organisms/NavigationBar";
+import { beforeEach, describe, expect, test } from "vitest";
 
-describe("<NavigationBar/>", () => {});
+describe("<NavigationBar/>", () => {
+    beforeEach(() => {
+        render(<NavigationBar />);
+    });
+
+    test("has header", () => {
+        expect(screen.getByRole("banner")).toBeVisible();
+    });
+});
