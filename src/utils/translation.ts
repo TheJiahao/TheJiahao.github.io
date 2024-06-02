903af63f81429a20e4f6a00a0b099758e2b9893d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-02 11:06:23 +0300

    refactor(#54): Import languages from `localization`

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -1,10 +1,6 @@
+import { languages } from "localization";
 import type { Language } from "../interfaces/Language";
-import en from "../localization/en";
-import zh_cn from "../localization/zh-cn";
 
-const languages = [en, zh_cn];
-
-const languageCodes = languages.map((language) => language.code);
 const translations: Record<string, Language> = Object.fromEntries(
     languages.map((language) => [language.code, language]),
 );
@@ -17,4 +13,4 @@ const getTranslation = (languageCode: string): Language => {
     return translations[languageCode];
 };
 
-export { getTranslation, languageCodes };
+export { getTranslation };
