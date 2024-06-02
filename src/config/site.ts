50972ae69a90b97dbcac5a29dd80255cfdf9737d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-02 11:17:27 +0300

    refactor(#54): Move site configs to own file

---
+++ src/config/site.ts
@@ -0,0 +1,19 @@
+const SITE_TITLE = "Jiahao 的博客";
+const SITE_DESCRIPTION =
+    "个人博客，内容可能涵盖工具配置、技术、数学等，不定时更新。";
+const SITE_OWNER = "Jiahao Li";
+const SITE_LICENSE = {
+    name: "CC BY-NC-SA 4.0",
+    url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+const SITE_START_YEAR = 2020;
+const SITE_SOURCE = "https://github.com/TheJiahao/TheJiahao.github.io";
+
+export {
+    SITE_DESCRIPTION,
+    SITE_LICENSE,
+    SITE_OWNER,
+    SITE_SOURCE,
+    SITE_START_YEAR,
+    SITE_TITLE,
+};
