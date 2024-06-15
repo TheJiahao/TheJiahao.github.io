fbd875945c0868d9c3c3815b729ed5fbaf287412
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-15 21:01:00 +0300

    refactor: Extract `MenuButton`

---
+++ src/components/atoms/MenuButton.tsx
@@ -0,0 +1,19 @@
+import useHydrationState from "hooks/useHydrationState";
+import type { AriaAttributes, ButtonHTMLAttributes } from "react";
+
+const MenuButton = (
+    props: AriaAttributes & ButtonHTMLAttributes<HTMLButtonElement>,
+) => (
+    <button
+        type="button"
+        className="i-lucide:menu"
+        disabled={!useHydrationState()}
+        {...props}
+        un-disabled="animate-spin i-lucide-loader-circle size-15"
+        justify-self-end
+        size-15
+        lg="hidden"
+    />
+);
+
+export default MenuButton;
--- src/components/molecules/NavigationToolBar.tsx
+++ src/components/molecules/NavigationToolBar.tsx
@@ -1,7 +1,7 @@
 import { getRelativeLocaleUrl } from "astro:i18n";
 import Avatar from "components/atoms/Avatar";
+import MenuButton from "components/atoms/MenuButton";
 import { DEFAULT_LANGUAGE, SITE_AVATAR } from "config";
-import useHydrationState from "hooks/useHydrationState";
 import type { AriaAttributes, HTMLAttributes, MouseEventHandler } from "react";
 import { getTranslation } from "utils/getTranslation";
 
@@ -19,40 +19,21 @@ const NavigationToolBar = ({
     language = DEFAULT_LANGUAGE,
     role = "toolbar",
     ...props
-}: NavigationToolBarProps) => {
-    const disabled = !useHydrationState();
+}: NavigationToolBarProps) => (
+    <div role={role} {...props} w-full bg-white grid grid-cols-3 items-center>
+        <Avatar
+            image={avatar}
+            size="15"
+            url={getRelativeLocaleUrl(language, "/about")}
+        />
 
-    return (
-        <div
-            role={role}
-            {...props}
-            w-full
-            bg-white
-            grid
-            grid-cols-3
-            items-center
-        >
-            <Avatar
-                image={avatar}
-                size="15"
-                url={getRelativeLocaleUrl(language, "/about")}
-            />
+        <div />
 
-            <div />
-
-            <button
-                type="button"
-                className="i-lucide:menu"
-                disabled={disabled}
-                onClick={handleExpand}
-                aria-label={getTranslation(language).showNavigationMenu}
-                un-disabled="animate-spin i-lucide-loader-circle size-15"
-                justify-self-end
-                size-15
-                lg="hidden"
-            />
-        </div>
-    );
-};
+        <MenuButton
+            onClick={handleExpand}
+            aria-label={getTranslation(language).showNavigationMenu}
+        />
+    </div>
+);
 
 export default NavigationToolBar;
