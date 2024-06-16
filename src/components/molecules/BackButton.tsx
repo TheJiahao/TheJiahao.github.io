cfbe3a02e6955b0536906f278736af23c7cc9e22
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-16 17:00:19 +0300

    feat(BackButton): Allow setting font size

--- src/components/molecules/BackButton.tsx
+++ src/components/molecules/BackButton.tsx
@@ -5,14 +5,22 @@ import { getTranslation } from "utils/getTranslation";
 interface BackButtonProps {
     language: string;
     label?: string;
+    fontSize?: string;
 }
 
 const BackButton = ({
     language,
     label = getTranslation(language).back,
+    fontSize = "lg",
 }: BackButtonProps) => {
     return (
-        <a href={getRelativeLocaleUrl(language, "/")} inline-flex card p-2>
+        <a
+            href={getRelativeLocaleUrl(language, "/")}
+            inline-flex
+            card
+            p-2
+            text={fontSize}
+        >
             <IconComponent icon="i-lucide:chevron-left">{label}</IconComponent>
         </a>
     );
