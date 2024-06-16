d2e49b82109b52b7c69b6f5ee5764f90c38b229d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-16 11:16:05 +0300

    test(#38): Check avatar link

--- src/tests/components/molecules/NavigationToolBar.test.tsx
+++ src/tests/components/molecules/NavigationToolBar.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { getRelativeLocaleUrl } from "astro:i18n";
 import NavigationToolBar from "components/molecules/NavigationToolBar";
 import { getTranslation } from "utils/getTranslation";
 import { beforeEach, describe, expect, test, vi } from "vitest";
@@ -14,6 +15,14 @@ describe("<NavigationToolBar/>", () => {
                 name: getTranslation("zh-cn").siteAvatar,
             }),
         ).toBeInTheDocument();
+    });
+
+    test("avatar links to about page", () => {
+        expect(
+            screen.getByRole("link", {
+                name: getTranslation("zh-cn").siteAvatar,
+            }),
+        ).toHaveAttribute("href", getRelativeLocaleUrl("zh-cn", "/about"));
     });
 
     test("has menu button", () => {
