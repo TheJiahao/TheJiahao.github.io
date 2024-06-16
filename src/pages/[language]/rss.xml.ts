e74b7d4b0d5a21880295ddfa98ec23e87de70e1d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-16 20:56:23 +0300

    feat(#36): Show full content in RSS

--- src/pages/[language]/rss.xml.ts
+++ src/pages/[language]/rss.xml.ts
@@ -2,6 +2,8 @@ import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
 import { getAbsoluteLocaleUrl } from "astro:i18n";
 import { languageCodes } from "localization";
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
 import getBlogs from "utils/getBlogs";
 import { getTranslation } from "utils/getTranslation";
 
@@ -12,6 +14,7 @@ export function getStaticPaths() {
 export function GET({ params }: APIContext) {
     const language = params.language;
     const blogs = getBlogs();
+    const parser = new MarkdownIt();
 
     if (!language) {
         throw new TypeError("Invalid language");
@@ -21,11 +24,14 @@ export function GET({ params }: APIContext) {
         title: getTranslation(language).siteTitle,
         description: getTranslation(language).siteDescription,
         site: getAbsoluteLocaleUrl(language, "/"),
-        items: blogs.map(({ data, slug }) => ({
+        items: blogs.map(({ data, slug, body }) => ({
             title: data.title,
             description: data.description,
             link: getAbsoluteLocaleUrl(language, `posts/${slug}`),
             pubDate: data.date,
+            content: sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+            }),
         })),
         customData: `<language>${language}</language>`,
     });
