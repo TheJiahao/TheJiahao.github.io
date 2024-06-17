b6890c657c637594723ac2302fea0f34b7ab696d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-17 20:52:37 +0300

    feat(social): Add link to GitHub profile

--- src/config/social.ts
+++ src/config/social.ts
@@ -2,6 +2,11 @@ import type { SocialLinkProps } from "components/atoms/SocialLink";
 
 const SOCIAL_LINKS: SocialLinkProps[] = [
     {
+        url: "https://github.com/TheJiahao",
+        icon: "i-logos-github-icon",
+        title: "GitHub",
+    },
+    {
         url: "/rss.xml",
         icon: "i-lucide-rss",
         title: "RSS",
