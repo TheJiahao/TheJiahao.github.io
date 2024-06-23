85d0e5b0f2ff6c95039e9e4c937b50dbd7e30d55
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-23 16:19:07 +0300

    fix(e2e): Blog navigation tests

--- src/tests/e2e/blog_navigation.test.tsx
+++ src/tests/e2e/blog_navigation.test.tsx
@@ -1,38 +1,52 @@
 import { expect, type Locator } from "@playwright/test";
+import { languageCodes } from "localization";
+import { getTranslation } from "utils/getTranslation";
 import test from "./utils/fixtures";
 
 test.describe("Blog navigation", () => {
     let blogList: Locator;
 
-    test.beforeEach(async ({ homepage, page }) => {
-        await homepage.goto();
-        blogList = page.getByRole("list", { name: "List of blogs" });
-    });
+    for (const languageCode of languageCodes) {
+        test.describe(`in ${languageCode} site`, () => {
+            test.use({ languageCode });
 
-    test("exists", async () => {
-        await expect(blogList).toBeVisible();
-    });
+            test.beforeEach(async ({ homepage, page }) => {
+                await homepage.goto();
+                blogList = page.getByRole("list", {
+                    name: getTranslation(languageCode).blogList,
+                });
+            });
 
-    test("has at least one blog", async () => {
-        const blogs = blogList.getByRole("listitem");
-        await expect(blogs).not.toHaveCount(0);
-    });
+            test("exists", async () => {
+                await expect(blogList).toBeVisible();
+            });
 
-    test.describe("after clicking blog card", () => {
-        test.beforeEach(async () => {
-            await blogList.getByRole("listitem").first().click();
+            test("has at least one blog", async () => {
+                const blogs = blogList.getByRole("listitem");
+                await expect(blogs).not.toHaveCount(0);
+            });
+
+            test.describe("after clicking blog card", () => {
+                test.beforeEach(async () => {
+                    await blogList.getByRole("listitem").first().click();
+                });
+
+                test("navigates to blog post", async ({ page }) => {
+                    await expect(page).toHaveURL(/\/posts\//);
+                });
+
+                test("clicking back button leads back to homepage", async ({
+                    page,
+                }) => {
+                    await page
+                        .getByRole("link", {
+                            name: getTranslation(languageCode).back,
+                        })
+                        .click();
+
+                    await expect(page).toHaveURL(RegExp(`/${languageCode}/?`));
+                });
+            });
         });
-
-        test("navigates to blog post", async ({ page }) => {
-            await expect(page).toHaveURL(/\/posts\//);
-        });
-
-        test("clicking back button leads back to homepage", async ({
-            page,
-        }) => {
-            await page.getByRole("link", { name: "返回" }).click();
-
-            await expect(page).toHaveURL(/\/zh-cn\/$/);
-        });
-    });
+    }
 });
