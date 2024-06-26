ef899a952812449de4a4d8b90d4b58cec8380ca5
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-26 20:30:51 +0300

    feat(BlogDetails): Add calendar icon

--- src/components/molecules/BlogDetails.tsx
+++ src/components/molecules/BlogDetails.tsx
@@ -1,3 +1,5 @@
+import IconComponent from "components/atoms/IconComponent";
+import { LuCalendar } from "react-icons/lu";
 import formatDate from "utils/formatDate";
 
 interface BlogDetailsProps {
@@ -10,7 +12,11 @@ const BlogDetails = ({ title, description, date }: BlogDetailsProps) => (
     <div prose px-8 py-2 max-w-full>
         <h2>{title}</h2>
         {description && <p text="slate lg">{description}</p>}
-        <time>{formatDate(date)}</time>
+        <IconComponent
+            icon={<LuCalendar role="presentation" focusable="false" />}
+        >
+            <time>{formatDate(date)}</time>
+        </IconComponent>
     </div>
 );
 
