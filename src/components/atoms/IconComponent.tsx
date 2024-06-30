4963bce8c268b7f6e07705cb2769ce5ef8c25a34
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-30 17:32:03 +0300

    feat(#51): Allow passing color to IconComponent

--- src/components/atoms/IconComponent.tsx
+++ src/components/atoms/IconComponent.tsx
@@ -2,10 +2,15 @@ import type { PropsWithChildren, ReactElement } from "react";
 
 interface IconComponentProps extends PropsWithChildren {
     icon?: ReactElement;
+    color?: string;
 }
 
-const IconComponent = ({ icon, children }: IconComponentProps) => (
-    <span flex items-center gap-4>
+const IconComponent = ({
+    icon,
+    color = "secondary",
+    children,
+}: IconComponentProps) => (
+    <span flex items-center gap-4 text={color}>
         {icon}
         <span>{children}</span>
     </span>
