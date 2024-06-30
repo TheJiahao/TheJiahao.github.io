e2dd237d441e612c53d63cdaf5192faaa08a7328
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-30 20:19:19 +0300

    refactor(semantic): Wrap `LinkCard` into article

--- src/components/organisms/LinkCard.tsx
+++ src/components/organisms/LinkCard.tsx
@@ -7,23 +7,26 @@ interface LinkProps {
 
 const LinkCard = ({ title, description, url, image }: LinkProps) => {
     return (
-        <a href={url} flex gap-4 p-8>
-            <div flex-auto leading-loose>
-                <h2 text="primary xl" font-bold>
-                    {title}
-                </h2>
-                <p text-secondary>{description}</p>
-            </div>
-            <img
-                src={image}
-                alt={title}
-                width="50vh"
-                height="50vh"
-                my-auto
-                flex-none
-                object-contain
-            />
-        </a>
+        <article title={title}>
+            <a href={url} flex="~ row" w-full p-8>
+                <div flex-auto leading-loose>
+                    <h1 text="primary xl" font-bold>
+                        {title}
+                    </h1>
+                    <p text-secondary>{description}</p>
+                </div>
+
+                <img
+                    src={image}
+                    alt={title}
+                    width="50vh"
+                    height="50vh"
+                    my-auto
+                    flex-none
+                    object-contain
+                />
+            </a>
+        </article>
     );
 };
 
