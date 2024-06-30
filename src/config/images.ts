651973444f18ef6afc3944ada693b0a59c204253
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-30 15:43:24 +0300

    refactor(config): Use src as import root

--- src/config/images.ts
+++ src/config/images.ts
@@ -1,4 +1,4 @@
-import SITE_AVATAR from "/src/assets/images/avatar.svg";
-import BLOG_IMAGE_PLACEHOLDER from "/src/assets/images/cover_placeholder.svg";
+import SITE_AVATAR from "assets/images/avatar.svg";
+import BLOG_IMAGE_PLACEHOLDER from "assets/images/cover_placeholder.svg";
 
 export { BLOG_IMAGE_PLACEHOLDER, SITE_AVATAR };
