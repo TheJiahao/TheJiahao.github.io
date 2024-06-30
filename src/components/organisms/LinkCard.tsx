458de055841f121337a8cb45ff6d437e983fdda5
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-06-30 21:15:01 +0300

    fix(LinkCard): Remove `console.log`

--- src/components/organisms/LinkCard.tsx
+++ src/components/organisms/LinkCard.tsx
@@ -5,34 +5,30 @@ interface LinkProps {
     image?: ImageMetadata | string;
 }
 
-const LinkCard = ({ title, description, url, image }: LinkProps) => {
-    console.log(image);
+const LinkCard = ({ title, description, url, image }: LinkProps) => (
+    <article title={title}>
+        <a href={url} flex="~ row" w-full p-8>
+            <div flex-auto leading-loose>
+                <h1 text="primary xl" font-bold>
+                    {title}
+                </h1>
+                <p text-secondary>{description}</p>
+            </div>
 
-    return (
-        <article title={title}>
-            <a href={url} flex="~ row" w-full p-8>
-                <div flex-auto leading-loose>
-                    <h1 text="primary xl" font-bold>
-                        {title}
-                    </h1>
-                    <p text-secondary>{description}</p>
-                </div>
-
-                {image && (
-                    <img
-                        src={typeof image == "string" ? image : image.src}
-                        alt={title}
-                        width="50vh"
-                        height="50vh"
-                        my-auto
-                        flex-none
-                        object-contain
-                    />
-                )}
-            </a>
-        </article>
-    );
-};
+            {image && (
+                <img
+                    src={typeof image == "string" ? image : image.src}
+                    alt={title}
+                    width="50vh"
+                    height="50vh"
+                    my-auto
+                    flex-none
+                    object-contain
+                />
+            )}
+        </a>
+    </article>
+);
 
 export type { LinkProps };
 export default LinkCard;
