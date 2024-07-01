fc6bb245fda3b93f46540bab15d26b8c5038e62e
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-01 19:51:31 +0300

    refactor(#51): Move prose CSS overrides to SCSS file

--- uno.config.ts
+++ uno.config.ts
@@ -14,26 +14,7 @@ export default defineConfig({
         presetUno({
             dark: "media",
         }),
-        presetTypography({
-            cssExtend: {
-                ":not(pre) > code::before,:not(pre) > code::after": {
-                    content: "unset",
-                },
-                ":not(pre) > code": {
-                    "background-color": "#f2f1f1",
-                    padding: "0.3em 0.2em",
-                    "border-radius": "0.375em",
-                    color: "darkGreen",
-                },
-                a: {
-                    "text-decoration": "underline 2px",
-                    "text-underline-offset": "0.25em",
-                },
-                ".data-footnote-backref": {
-                    "text-decoration": "none",
-                },
-            },
-        }),
+        presetTypography(),
         presetWebFonts({
             provider: "bunny",
             fonts: {
