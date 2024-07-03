986bb977abfacd9edd0dc84ec2d646ed1bc30001
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-03 19:11:26 +0300

    fix(test): Add mock for ResizeObserver

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,6 +1,16 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { afterEach, beforeAll, vi } from "vitest";
+
+beforeAll(() => {
+    const ResizeObserverMock = vi.fn(() => ({
+        observe: vi.fn(),
+        unobserve: vi.fn(),
+        disconnect: vi.fn(),
+    }));
+
+    vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+});
 
 afterEach(() => {
     cleanup();
