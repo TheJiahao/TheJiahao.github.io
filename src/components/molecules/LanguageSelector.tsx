8493a9448f3ec0b74066236248c7e36b7d3c6596
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-03 17:32:54 +0300

    refactor(#51): Use `<Listbox/>` in LanguageSelector

--- src/components/molecules/LanguageSelector.tsx
+++ src/components/molecules/LanguageSelector.tsx
@@ -1,9 +1,15 @@
+import {
+    Listbox,
+    ListboxButton,
+    ListboxOption,
+    ListboxOptions,
+} from "@headlessui/react";
 import { getRelativeLocaleUrl } from "astro:i18n";
 import IconComponent from "components/atoms/IconComponent";
 import type { Language } from "interfaces/Language";
 import { languages as DEFAULT_LANGUAGES } from "localization";
-import { useEffect, useState, type ChangeEventHandler } from "react";
-import { LuGlobe } from "react-icons/lu";
+import { useEffect, useState } from "react";
+import { LuChevronDown, LuGlobe } from "react-icons/lu";
 import { getTranslation } from "utils/getTranslation";
 
 interface LanguageSelectorProps {
@@ -21,8 +27,8 @@ const LanguageSelector = ({
         setDisabled(false);
     }, []);
 
-    const handleNavigation: ChangeEventHandler<HTMLSelectElement> = (event) => {
-        const language = event.target.value;
+    const handleNavigation = (value: string) => {
+        const language = value;
 
         setDisabled(true);
         window.location.href = getRelativeLocaleUrl(language, "/");
@@ -30,18 +36,42 @@ const LanguageSelector = ({
 
     return (
         <IconComponent icon={<LuGlobe />}>
-            <select
-                aria-label={getTranslation(defaultLanguage).selectLanguage}
-                defaultValue={defaultLanguage}
-                onChange={handleNavigation}
-                disabled={disabled}
-            >
-                {languages.map(({ name, code }) => (
-                    <option key={code} value={code}>
-                        {name}
-                    </option>
-                ))}
-            </select>
+            <div>
+                <Listbox
+                    value={defaultLanguage}
+                    onChange={handleNavigation}
+                    disabled={disabled}
+                >
+                    <ListboxButton
+                        aria-label={
+                            getTranslation(defaultLanguage).selectLanguage
+                        }
+                    >
+                        <span inline-flex items-center gap-2>
+                            {getTranslation(defaultLanguage).name}
+                            <LuChevronDown aria-hidden />
+                        </span>
+                    </ListboxButton>
+                    <ListboxOptions
+                        className="[--anchor-gap:1rem]"
+                        anchor="bottom end"
+                        card
+                        flex="~ col"
+                        gap-1
+                    >
+                        {languages.map(({ name, code }) => (
+                            <ListboxOption
+                                key={code}
+                                value={code}
+                                p-2
+                                hover="cursor-pointer bg-surface-tertiary"
+                            >
+                                {name}
+                            </ListboxOption>
+                        ))}
+                    </ListboxOptions>
+                </Listbox>
+            </div>
         </IconComponent>
     );
 };
