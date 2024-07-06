8b704b789ffd431f235a1af460c6118c4cf36267
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-06 15:23:27 +0300

    feat(#5): Add `getTOC` for nesting headings

--- src/tests/utils/getTOC.test.ts
+++ src/tests/utils/getTOC.test.ts
@@ -5,19 +5,25 @@ import { describe, expect, test } from "vitest";
 describe("getTOC()", () => {
     test("nests two level headings", () => {
         const headings: MarkdownHeading[] = [
-            { depth: 2, text: "Introduction", slug: "introduction" },
+            {
+                depth: 2,
+                text: "Introduction",
+                slug: "introduction",
+            },
             { depth: 3, text: "Background", slug: "background" },
             { depth: 3, text: "Highlight", slug: "highlight" },
-            { depth: 2, text: "Features", slug: "features" },
+            {
+                depth: 2,
+                text: "Features",
+                slug: "features",
+            },
             { depth: 3, text: "Feature 1", slug: "feature-1" },
             { depth: 3, text: "Feature 2", slug: "feature-2" },
         ];
 
         expect(getTOC(headings)).toEqual([
             {
-                depth: 2,
-                text: "Introduction",
-                slug: "introduction",
+                ...headings[0],
                 subHeadings: [
                     {
                         depth: 3,
@@ -31,27 +37,69 @@ describe("getTOC()", () => {
                         slug: "highlight",
                         subHeadings: [],
                     },
+                ],
+            },
+            {
+                ...headings[3],
+                subHeadings: [
                     {
-                        depth: 2,
-                        text: "Features",
-                        slug: "features",
-                        subHeadings: [
-                            {
-                                depth: 3,
-                                text: "Feature 1",
-                                slug: "feature-1",
-                                subHeadings: [],
-                            },
-                            {
-                                depth: 3,
-                                text: "Feature 2",
-                                slug: "feature-2",
-                                subHeadings: [],
-                            },
-                        ],
+                        depth: 3,
+                        text: "Feature 1",
+                        slug: "feature-1",
+                        subHeadings: [],
+                    },
+                    {
+                        depth: 3,
+                        text: "Feature 2",
+                        slug: "feature-2",
+                        subHeadings: [],
                     },
                 ],
             },
         ]);
+    });
+
+    test("nests three level headings", () => {
+        const headings: MarkdownHeading[] = [
+            {
+                depth: 2,
+                text: "Introduction",
+                slug: "introduction",
+            },
+            { depth: 3, text: "Background", slug: "background" },
+            { depth: 4, text: "Sub Background", slug: "sub-background" },
+            { depth: 3, text: "Highlight", slug: "highlight" },
+        ];
+
+        expect(getTOC(headings)[0].subHeadings).toEqual([
+            {
+                depth: 3,
+                text: "Background",
+                slug: "background",
+                subHeadings: [
+                    {
+                        depth: 4,
+                        text: "Sub Background",
+                        slug: "sub-background",
+                        subHeadings: [],
+                    },
+                ],
+            },
+            {
+                depth: 3,
+                text: "Highlight",
+                slug: "highlight",
+                subHeadings: [],
+            },
+        ]);
+    });
+
+    test("detects missing parent", () => {
+        const headings: MarkdownHeading[] = [
+            { depth: 2, text: "Introduction", slug: "introduction" },
+            { depth: 4, text: "Subsubheading", slug: "subsubheading" },
+        ];
+
+        expect(() => getTOC(headings)).toThrowError(/Subsubheading/);
     });
 });
--- src/utils/getTOC.ts
+++ src/utils/getTOC.ts
@@ -1,4 +1,32 @@
 import type { MarkdownHeading } from "astro";
 import type { Heading } from "interfaces/Heading";
 
-export const getTOC = (headings: MarkdownHeading[]): Heading[] => {};
+export const getTOC = (
+    headings: MarkdownHeading[],
+    startDepth: number = 2,
+): Heading[] => {
+    const result: Heading[] = [];
+    const parentHeadings = new Array<Heading | null>(7).fill(null);
+
+    for (const h of headings) {
+        const heading = { ...h, subHeadings: [] };
+        parentHeadings[heading.depth] = heading;
+
+        if (heading.depth === startDepth) {
+            result.push(heading);
+            continue;
+        }
+
+        const parent = parentHeadings[heading.depth - 1];
+
+        if (!parent) {
+            throw new Error(
+                `Heading ${h.text} missing parent. Headings should only decrease by one level`,
+            );
+        }
+
+        parent.subHeadings.push(heading);
+    }
+
+    return result;
+};
