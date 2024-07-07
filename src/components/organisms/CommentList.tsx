2bbbaec19c7150c2fc71c761f3c4c3bff7c9ba16
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-07 12:31:13 +0300

    docs(#111): Add JSDoc

--- src/components/organisms/CommentList.tsx
+++ src/components/organisms/CommentList.tsx
@@ -2,6 +2,12 @@ import Giscus from "@giscus/react";
 import { GISCUS_CONFIG } from "config";
 import type { TranslatedElement } from "interfaces/TranslatedElement";
 
+/**
+ * Component for comments.
+ *
+ * @param {string} language RFC 5646 language code with language tag and country code like zh-cn or en-us
+ * @see config.comments for configuration
+ */
 const CommentList = ({ language }: TranslatedElement) => {
     if (language.includes("-")) {
         const [languageTag, countryCode] = language.split("-");
