784a0573ac7d2d86332dfe3ffc4572546e9c7c5e
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-07 12:23:59 +0300

    fix(#111): Capitalize country code in language

--- src/components/organisms/CommentList.tsx
+++ src/components/organisms/CommentList.tsx
@@ -3,6 +3,12 @@ import { GISCUS_CONFIG } from "config";
 import type { TranslatedElement } from "interfaces/TranslatedElement";
 
 const CommentList = ({ language }: TranslatedElement) => {
+    if (language.includes("-")) {
+        const [languageTag, countryCode] = language.split("-");
+
+        language = [languageTag, countryCode.toUpperCase()].join("-");
+    }
+
     return <Giscus {...GISCUS_CONFIG} lang={language} />;
 };
 
