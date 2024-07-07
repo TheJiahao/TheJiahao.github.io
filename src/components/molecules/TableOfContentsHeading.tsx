8b68f060f6597a20fd8cc74997c0b53bdbd9aace
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-07 09:23:08 +0300

    refactor(#5): Extract useVisibleSection

--- src/components/molecules/TableOfContentsHeading.tsx
+++ src/components/molecules/TableOfContentsHeading.tsx
@@ -1,51 +1,12 @@
+import useVisibleSection from "hooks/useVisibleSection";
 import type { Heading } from "interfaces/Heading";
-import { useEffect } from "react";
 
 interface HeadingListProps {
     heading: Heading;
 }
 
 const TableOfContentsHeading = ({ heading }: HeadingListProps) => {
-    useEffect(() => {
-        const articleSections =
-            document.querySelectorAll<HTMLDivElement>("article section");
-
-        const observer = new IntersectionObserver((entries) => {
-            entries.map((entry) => {
-                const heading =
-                    entry.target.querySelector<HTMLHeadingElement>(
-                        "h2,h3,h4,h5,h6",
-                    );
-
-                if (!heading) return;
-
-                const id = heading.getAttribute("id");
-
-                if (!id) return;
-
-                const link = document.querySelector<HTMLAnchorElement>(
-                    `nav > * a[href="#${id}"]`,
-                );
-
-                if (!link) return;
-
-                const addRemove =
-                    entry.intersectionRatio > 0 ? "add" : "remove";
-                link.classList[addRemove](
-                    "text-blue-500",
-                    "dark:text-blue-400",
-                );
-            });
-        });
-
-        for (const section of articleSections) {
-            observer.observe(section);
-        }
-
-        window.document.addEventListener("beforeunload", () => {
-            observer.disconnect();
-        });
-    }, []);
+    useVisibleSection();
 
     return (
         <li key={heading.slug}>
