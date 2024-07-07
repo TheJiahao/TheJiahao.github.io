d408bd686635d21320d76f1f367ea382d67d7257
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-07 10:40:54 +0300

    fix(test): Mock Intersection Observer

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -9,7 +9,15 @@ beforeAll(() => {
         disconnect: vi.fn(),
     }));
 
+    const IntersectionObserverMock = vi.fn(() => ({
+        disconnect: vi.fn(),
+        observe: vi.fn(),
+        takeRecords: vi.fn(),
+        unobserve: vi.fn(),
+    }));
+
     vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+    vi.stubGlobal(`IntersectionObserver`, IntersectionObserverMock);
 });
 
 afterEach(() => {
