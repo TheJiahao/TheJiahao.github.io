32be376ecb11e1644e5ce65a2a4ea045ad7b70c0
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-07 10:54:24 +0300

    feat(#111): Add giscus config

---
+++ src/config/comments.ts
@@ -0,0 +1,11 @@
+import type { GiscusProps } from "@giscus/react";
+
+export const GISCUS_CONFIG: GiscusProps = {
+    repo: "TheJiahao/TheJiahao.github.io",
+    repoId: "MDEwOlJlcG9zaXRvcnkyOTc3MDMxMjY=",
+    category: "Comments",
+    mapping: "pathname",
+    theme: "preferred_color_scheme",
+    reactionsEnabled: "1",
+    emitMetadata: "0",
+};
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
+export * from "config/comments";
 export * from "config/images";
 export * from "config/languages";
 export * from "config/navigation";
