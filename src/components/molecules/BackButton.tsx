8018c6f4d6beed85d1e02f9dec4c96393bcf5c7d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-13 17:46:37 +0300

    feat: ✨ Add click effects to back button
    Refs: #116

--- src/components/molecules/BackButton.tsx
+++ src/components/molecules/BackButton.tsx
@@ -19,6 +19,7 @@ const BackButton = ({
             card
             p-2
             text="secondary lg"
+            clickable
         >
             <IconComponent icon={<LuChevronLeft />}>{label}</IconComponent>
         </a>
