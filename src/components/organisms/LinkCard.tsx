0d4b82c4061d2cbba7a34b56cd251545b27fde5a
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-13 17:37:38 +0300

    fix: 🐛 Open link card in new tab

--- src/components/organisms/LinkCard.tsx
+++ src/components/organisms/LinkCard.tsx
@@ -7,7 +7,14 @@ interface LinkProps {
 
 const LinkCard = ({ title, description, url, image }: LinkProps) => (
     <article title={title} clickable>
-        <a href={url} flex="~ row" w-full p-8>
+        <a
+            href={url}
+            flex="~ row"
+            w-full
+            p-8
+            target="_blank"
+            rel="noreferrer noopener"
+        >
             <div flex-auto leading-loose>
                 <h1 text="primary xl" font-bold>
                     {title}
