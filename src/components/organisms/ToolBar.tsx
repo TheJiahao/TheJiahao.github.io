b8c6f0201591d2d6c9581a131bc6dbf9b5b0f2a8
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-16 20:19:43 +0300

    feat(search): ✨ Add search box to toolbar
    Refs: #53

--- src/components/organisms/ToolBar.tsx
+++ src/components/organisms/ToolBar.tsx
@@ -1,9 +1,11 @@
 import LanguageSelector from "components/molecules/LanguageSelector";
+import SearchBox from "components/molecules/SearchBox";
 import type { TranslatedElement } from "interfaces/TranslatedElement";
 
 const ToolBar = ({ language }: TranslatedElement) => {
     return (
-        <aside card mx-auto p-2>
+        <aside card mx-auto p-2 flex="~ row" gap-lg>
+            <SearchBox />
             <LanguageSelector defaultLanguage={language} />
         </aside>
     );
