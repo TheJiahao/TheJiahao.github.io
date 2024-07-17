3b0d1df4e464b68b10e7a287f2942f0432123445
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-17 20:17:52 +0300

    feat(search): ✨ Add `search`
    Refs: #53

---
+++ src/utils/search.ts
@@ -0,0 +1,13 @@
+import fuzzysort from "fuzzysort";
+import type { Page } from "interfaces/Page";
+
+const search = (keyword: string, pages: Page[]): Page[] => {
+    return fuzzysort
+        .go(keyword, pages, {
+            limit: 5,
+            keys: ["title", "content"],
+        })
+        .map((result) => result.obj);
+};
+
+export { search };
