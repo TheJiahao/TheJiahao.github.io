c1717aa5afc7b070183571e627493bdc1d9d9e16
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-17 20:31:47 +0300

    feat(search): ✨ Add index file
    Refs: #53

---
+++ src/pages/[language]/search-index.json.ts
@@ -0,0 +1,27 @@
+import type { APIContext } from "astro";
+import { getAbsoluteLocaleUrl } from "astro:i18n";
+import { languageCodes } from "localization";
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
+import getBlogs from "utils/getBlogs";
+
+export function getStaticPaths() {
+    return languageCodes.map((language) => ({ params: { language } }));
+}
+
+export function GET({ params }: APIContext) {
+    const language = params.language;
+    const parser = new MarkdownIt();
+
+    if (!language) {
+        throw new TypeError("Invalid language");
+    }
+
+    const blogs = getBlogs().map(({ data, slug, body }) => ({
+        title: data.title,
+        url: getAbsoluteLocaleUrl(language, `posts/${slug}`),
+        content: sanitizeHtml(parser.render(body)),
+    }));
+
+    return new Response(JSON.stringify(blogs));
+}
