5bee1de4ef1fb6c89a33b970db62168638a68a6c
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-20 14:55:25 +0300

    fix(search): 🐛 Add image to index
    Refs: #53

--- src/pages/[language]/search-index.json.ts
+++ src/pages/[language]/search-index.json.ts
@@ -1,5 +1,6 @@
 import type { APIContext } from "astro";
 import { getAbsoluteLocaleUrl } from "astro:i18n";
+import { BLOG_IMAGE_PLACEHOLDER } from "config";
 import { languageCodes } from "localization";
 import MarkdownIt from "markdown-it";
 import sanitizeHtml from "sanitize-html";
@@ -23,6 +24,7 @@ export function GET({ params }: APIContext) {
             title: data.title,
             url: getAbsoluteLocaleUrl(data.language, `posts/${slug}`),
             content: sanitizeHtml(parser.render(body)),
+            image: data.image ?? BLOG_IMAGE_PLACEHOLDER,
         }));
 
     return new Response(JSON.stringify(blogs));
