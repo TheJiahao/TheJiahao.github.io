caceea70af283132a66525da06464bcfeeefaa38
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-21 15:37:18 +0300

    refactor(search): ♻️ Replace search button with label
    Refs: #53

--- src/components/molecules/SearchBox.tsx
+++ src/components/molecules/SearchBox.tsx
@@ -2,6 +2,7 @@ import LinkList from "components/organisms/LinkList";
 import useSearch from "hooks/useSearch";
 import type { TranslatedElement } from "interfaces/TranslatedElement";
 import { useState, type ChangeEventHandler } from "react";
+import { LuSearch } from "react-icons/lu";
 
 const SearchBox = ({ language }: TranslatedElement) => {
     const [keyword, setKeyword] = useState("");
@@ -21,8 +22,16 @@ const SearchBox = ({ language }: TranslatedElement) => {
 
     return (
         <div>
-            <form align-icon gap-sm>
-                <input type="search" value={keyword} onChange={onChange} />
+            <form align-icon gap-1>
+                <label htmlFor="search">
+                    <LuSearch />
+                </label>
+                <input
+                    type="search"
+                    id="search"
+                    value={keyword}
+                    onChange={onChange}
+                />
             </form>
             <LinkList language={language} links={searchResult} />
         </div>
