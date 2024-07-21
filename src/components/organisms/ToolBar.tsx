202c7fdebf0862bc6f39b1585066f94d22b285fd
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-21 16:17:51 +0300

    refactor(search): ♻️ Move search box outside toolbar
    Refs: #53

--- src/components/organisms/ToolBar.tsx
+++ src/components/organisms/ToolBar.tsx
@@ -1,11 +1,9 @@
 import LanguageSelector from "components/molecules/LanguageSelector";
-import SearchBox from "components/molecules/SearchBox";
 import type { TranslatedElement } from "interfaces/TranslatedElement";
 
 const ToolBar = ({ language }: TranslatedElement) => {
     return (
         <aside card mx-auto p-2 flex="~ row" gap-lg>
-            <SearchBox language={language} />
             <LanguageSelector defaultLanguage={language} />
         </aside>
     );
