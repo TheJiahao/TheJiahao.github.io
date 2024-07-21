ea78d090c8f20aa25d0269cf4648f324088ac296
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-21 15:36:21 +0300

    refactor(search): ♻️ Use `LinkList` to show search results
    Refs: #53

--- src/components/molecules/SearchBox.tsx
+++ src/components/molecules/SearchBox.tsx
@@ -1,4 +1,4 @@
-import SearchButton from "components/atoms/SearchButton";
+import LinkList from "components/organisms/LinkList";
 import useSearch from "hooks/useSearch";
 import type { TranslatedElement } from "interfaces/TranslatedElement";
 import { useState, type ChangeEventHandler } from "react";
@@ -6,20 +6,26 @@ import { useState, type ChangeEventHandler } from "react";
 const SearchBox = ({ language }: TranslatedElement) => {
     const [keyword, setKeyword] = useState("");
 
-    const searchResult = useSearch(keyword, language);
+    const searchResult = useSearch(keyword, language).map(
+        ({ title, content, url, image }) => ({
+            title,
+            description: content,
+            url,
+            image,
+        }),
+    );
 
     const onChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
         setKeyword(target.value);
     };
 
     return (
-        <form align-icon gap-sm>
-            <input type="search" value={keyword} onChange={onChange} />
-            <SearchButton type="submit" />
-            {searchResult.map(({ title }) => (
-                <div key={title}>{title}</div>
-            ))}
-        </form>
+        <div>
+            <form align-icon gap-sm>
+                <input type="search" value={keyword} onChange={onChange} />
+            </form>
+            <LinkList language={language} links={searchResult} />
+        </div>
     );
 };
 
