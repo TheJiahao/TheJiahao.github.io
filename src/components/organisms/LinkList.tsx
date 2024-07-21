09fab0679917c27295f6dffde8fb7afdccea6561
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-21 17:17:51 +0300

    refactor: ♻️ Inject `card` to `LinkList`
    refs: #53

--- src/components/organisms/LinkList.tsx
+++ src/components/organisms/LinkList.tsx
@@ -1,20 +1,23 @@
 import LinkCard, { type LinkProps } from "components/organisms/LinkCard";
 import type { TranslatedElement } from "interfaces/TranslatedElement";
+import type { HTMLAttributes } from "react";
 import { getTranslation } from "utils/getTranslation";
 
-interface LinkListProps extends TranslatedElement {
+interface LinkListProps
+    extends TranslatedElement,
+        HTMLAttributes<HTMLUListElement> {
     links?: LinkProps[];
 }
 
-const LinkList = ({ links, language }: LinkListProps) => {
+const LinkList = ({ links, language, ...props }: LinkListProps) => {
     return (
         links && (
             <ul
                 aria-label={getTranslation(language).externalLinks}
-                card
                 w-full
                 mx-auto
                 divide-y
+                {...props}
             >
                 {links.map((link) => (
                     <li key={link.url}>
