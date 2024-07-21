edf5ab08bc8eec44d608d853a51ec48259ade821
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-21 17:56:47 +0300

    feat(search): ✨ Highlight keyword in result

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -8,7 +8,11 @@ const search = (keyword: string, pages: Page[]): Page[] => {
             threshold: 0.5,
             keys: ["title", "content"],
         })
-        .map((result) => result.obj);
+        .map((result) => ({
+            ...result.obj,
+            title: result[0].highlight("<strong bg-cyan-2>", "</strong>"),
+            content: result[1].highlight("<strong bg-cyan-2>", "</strong>"),
+        }));
 };
 
 export { search };
