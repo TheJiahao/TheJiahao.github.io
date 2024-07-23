78a2551726aa75b2eadfce0fdfc01058a653f713
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-23 21:15:26 +0300

    refactor(navbar): ♻️ Use `LoadingIcon`

--- src/components/atoms/MenuButton.tsx
+++ src/components/atoms/MenuButton.tsx
@@ -1,10 +1,11 @@
+import LoadingIcon from "components/atoms/LoadingIcon";
 import useHydrationState from "hooks/useHydrationState";
 import {
     cloneElement,
     type AriaAttributes,
     type ButtonHTMLAttributes,
 } from "react";
-import { LuLoader2, LuMenu, LuX } from "react-icons/lu";
+import { LuMenu, LuX } from "react-icons/lu";
 
 interface MenuButtonProps
     extends AriaAttributes,
@@ -15,15 +16,10 @@ interface MenuButtonProps
 const MenuButton = ({ expanded, ...props }: MenuButtonProps) => {
     const disabled = !useHydrationState();
 
-    const icon = disabled ? <LuLoader2 /> : expanded ? <LuX /> : <LuMenu />;
+    const icon = disabled ? <LoadingIcon /> : expanded ? <LuX /> : <LuMenu />;
 
     return (
-        <button
-            type="button"
-            disabled={disabled}
-            {...props}
-            un-disabled="animate-spin size-15"
-        >
+        <button type="button" disabled={disabled} {...props}>
             {cloneElement(icon, {
                 className: "size-15",
                 role: "presentation",
