7c1dc54b2183087f8dc214865189b9103bf24e96
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-23 21:14:20 +0300

    feat(loading-icon): ✨ Allow passing props

--- src/components/atoms/LoadingIcon.tsx
+++ src/components/atoms/LoadingIcon.tsx
@@ -1,7 +1,8 @@
+import type { IconBaseProps } from "react-icons/lib";
 import { LuLoader2 } from "react-icons/lu";
 
-const LoadingIcon = () => {
-    return <LuLoader2 role="presentation" focusable={false} animate-spin />;
+const LoadingIcon = ({ ...props }: IconBaseProps) => {
+    return <LuLoader2 animate-spin {...props} />;
 };
 
 export default LoadingIcon;
