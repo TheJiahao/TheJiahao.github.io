8278ad16bf37fc75e93df50f0a095533ed959ec9
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-23 21:08:30 +0300

    feat: ✨ Add `LoadingIcon`

---
+++ src/components/atoms/LoadingIcon.tsx
@@ -0,0 +1,7 @@
+import { LuLoader2 } from "react-icons/lu";
+
+const LoadingIcon = () => {
+    return <LuLoader2 role="presentation" focusable={false} animate-spin />;
+};
+
+export default LoadingIcon;
