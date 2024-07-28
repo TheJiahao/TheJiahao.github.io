7104769876bc7189ecf53f3bd37b69fd785bee47
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-28 11:42:14 +0300

    fix(language-selector): 🐛 Remove outline when focused

--- src/components/molecules/LanguageSelector.tsx
+++ src/components/molecules/LanguageSelector.tsx
@@ -66,7 +66,7 @@ const LanguageSelector = ({
             </ListboxButton>
 
             <ListboxOptions
-                className="data-[closed]:opacity-0"
+                className="data-[closed]:opacity-0 data-[focus]:outline-none"
                 anchor={{ to: "bottom end", gap: "1rem" }}
                 card
                 flex="~ col"
