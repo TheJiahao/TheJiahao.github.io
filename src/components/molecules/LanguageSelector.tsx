f9b12ad380b66469dd8f1fa8bc3f6eaf7f9953de
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-28 11:06:24 +0300

    fix: 🐛 Disable scroll when language selector is open

--- src/components/molecules/LanguageSelector.tsx
+++ src/components/molecules/LanguageSelector.tsx
@@ -68,7 +68,6 @@ const LanguageSelector = ({
             <ListboxOptions
                 className="data-[closed]:opacity-0"
                 anchor={{ to: "bottom end", gap: "1rem" }}
-                modal={false}
                 card
                 flex="~ col"
                 text-secondary
