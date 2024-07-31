a41b3a65920d92c414988367b083994ff8c7c8ea
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-31 17:50:18 +0300

    feat(portfolio): ✨ Add initial `ProjectList`
    Refs: #46

---
+++ src/components/organisms/ProjectList.tsx
@@ -0,0 +1,20 @@
+import type { BlogCardProps } from "components/molecules/BlogCard";
+import BlogCard from "components/molecules/BlogCard";
+import type { TranslatedElement } from "interfaces/TranslatedElement";
+import { getTranslation } from "utils/getTranslation";
+
+interface ProjectListProps extends TranslatedElement {
+    projects: BlogCardProps[];
+}
+
+const ProjectList = ({ projects, language }: ProjectListProps) => (
+    <ul aria-label={getTranslation(language).projects} flex="~ col" gap-10>
+        {projects.map((blog) => (
+            <li key={blog.title}>
+                <BlogCard {...blog} />
+            </li>
+        ))}
+    </ul>
+);
+
+export default ProjectList;
