2e15d4e76bfce622fa330c7dcdabab8323ab0e4b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-31 18:10:23 +0300

    feat(portfolio): ✨ Add project objects

---
+++ src/config/projects.ts
@@ -0,0 +1,54 @@
+import type { BlogCardProps } from "components/molecules/BlogCard";
+import { languageCodes } from "localization";
+
+interface RawProject extends Omit<BlogCardProps, "description"> {
+    description?: Record<string, string>;
+}
+
+const rawProjects: RawProject[] = [
+    {
+        title: "study-planner",
+        url: "https://github.com/TheJiahao/study-planner",
+        description: {
+            en: "Tool for generating compact schedules based on dependencies between courses and timing.",
+            "zh-cn": "基于课程之间依赖关系和教学时间的时间表生成工具。",
+        },
+    },
+    {
+        title: "kivi-sakset-paperi-tekoaly",
+        url: "https://github.com/TheJiahao/kivi-sakset-paperi-tekoaly",
+        description: {
+            en: "Markov chain-based AI for rock-paper-scissors.",
+            "zh-cn": "基于马尔可夫链的石头-剪刀-布 AI。",
+        },
+    },
+    {
+        title: "tira-ai-platform",
+        url: "https://github.com/game-ai-platform-team/tira-ai-platform",
+        description: {
+            en: "Tool for testing game AIs. Can run AIs on HPC or locally.",
+            "zh-cn": "游戏 AI 测试工具。可在 HPC 或本地运行 AI。",
+        },
+    },
+    {
+        title: "TheJiahao.github.io",
+        url: "https://thejiahao.github.io/",
+        description: {
+            en: "Blog built with Astro and React.",
+            "zh-cn": "用 Astro 和 React 构建的博客。",
+        },
+    },
+];
+
+const PROJECTS: Record<string, BlogCardProps[]> = Object.fromEntries(
+    languageCodes.map((language) => {
+        const projects = rawProjects.map(({ description, ...project }) => ({
+            ...project,
+            description: description?.[language],
+        }));
+
+        return [language, projects];
+    }),
+);
+
+export { PROJECTS };
