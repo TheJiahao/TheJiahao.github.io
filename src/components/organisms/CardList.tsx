b5a8ddcc53e57a345dbbde5715c02fc944e1b267
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-07-31 21:04:17 +0300

    refactor(card-list): ♻️ Use shorthand

--- src/components/organisms/CardList.tsx
+++ src/components/organisms/CardList.tsx
@@ -5,16 +5,14 @@ interface CardListProps extends AriaAttributes {
     blogs: ImageCardProps[];
 }
 
-const CardList = ({ blogs, ...props }: CardListProps) => {
-    return (
-        <ul flex="~ col" gap-10 {...props}>
-            {blogs.map((blog) => (
-                <li key={blog.title}>
-                    <ImageCard {...blog} />
-                </li>
-            ))}
-        </ul>
-    );
-};
+const CardList = ({ blogs, ...props }: CardListProps) => (
+    <ul flex="~ col" gap-10 {...props}>
+        {blogs.map((blog) => (
+            <li key={blog.title}>
+                <ImageCard {...blog} />
+            </li>
+        ))}
+    </ul>
+);
 
 export default CardList;
