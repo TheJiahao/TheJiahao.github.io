fe154c4afc4aa94659998df0d58e7fe785e53d3d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-08-01 19:44:44 +0300

    refactor(projects): ♻️ Store projects as json
    Refs: #202

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,5 @@ export * from "config/comments";
 export * from "config/images";
 export * from "config/languages";
 export * from "config/navigation";
-export * from "config/projects";
 export * from "config/site";
 export * from "config/social";
