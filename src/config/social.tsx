2b4e62a4dec2ac9086fc31df5d6eb8d690f0f6ed
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-08-03 17:47:32 +0300

    chore: 🔧 Add LinkedIn link

--- src/config/social.tsx
+++ src/config/social.tsx
@@ -1,12 +1,17 @@
 import type { SocialLinkProps } from "components/atoms/SocialLink";
 import { LuRss } from "react-icons/lu";
-import { SiGithub } from "react-icons/si";
+import { SiGithub, SiLinkedin } from "react-icons/si";
 
 const SOCIAL_LINKS: SocialLinkProps[] = [
     {
         url: "https://github.com/TheJiahao",
         icon: <SiGithub />,
         title: "GitHub",
+    },
+    {
+        url: "https://www.linkedin.com/in/jiahao-li-3380742a8",
+        icon: <SiLinkedin />,
+        title: "LinkedIn",
     },
     {
         url: "/rss.xml",
