a3bcf369a1ed68105fa0dc9c7b862cc7e6769d63
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-08-03 15:20:06 +0300

    revert: "feat(copy-button): ✨ Add initial copy button"
    This reverts commit c0c3e741ce01b4184b22cb77d129a749c20d3bcb.

--- src/components/atoms/CopyButton.tsx
+++
@@ -1,9 +0,0 @@
-import type { ButtonHTMLAttributes } from "react";
-
-const CopyButton = ({ ...props }: ButtonHTMLAttributes<HTMLButtonElement>) => (
-    <button type="button" {...props}>
-        Copy
-    </button>
-);
-
-export default CopyButton;
