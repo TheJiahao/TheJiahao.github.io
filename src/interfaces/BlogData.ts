33c251ee62801215490711d2bf7ada469cf08470
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-08-03 20:15:49 +0300

    feat(blog-multilanguage): ✨ Add `getAlternates`
    Refs:#199

--- src/interfaces/BlogData.ts
+++ src/interfaces/BlogData.ts
@@ -3,6 +3,7 @@ import type { CollectionEntry } from "astro:content";
 type BlogData = CollectionEntry<"posts">["data"] & {
     language: string;
     lastModified: Date;
+    alternates?: { language: string; slug: string }[];
 };
 
 export { type BlogData };
---
+++ src/utils/getAlternates.ts
@@ -0,0 +1,23 @@
+import type { BlogEntry } from "interfaces/BlogEntry";
+
+export const getAlternates = (
+    blogs: BlogEntry[],
+): Map<string, Set<{ language: string; slug: string }>> => {
+    const blogAlternates = new Map<
+        string,
+        Set<{ language: string; slug: string }>
+    >();
+
+    for (const blog of blogs) {
+        const alternates = blogAlternates.get(blog.slug) || new Set();
+
+        alternates.add({
+            language: blog.data.language,
+            slug: blog.slug,
+        });
+
+        blogAlternates.set(blog.slug, alternates);
+    }
+
+    return blogAlternates;
+};
