ea47432a8e7239772982d2171aa6692865b20150
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-08-04 12:46:25 +0300

    feat(blog-multilanguage): ✨ Add language selector if multiple languages available
    Refs: #199

--- src/components/molecules/BlogDetails.tsx
+++ src/components/molecules/BlogDetails.tsx
@@ -1,28 +1,56 @@
+import LanguageSelector from "components/molecules/LanguageSelector";
+import type { Multilingual } from "interfaces/Multilingual";
+import type { TranslatedElement } from "interfaces/TranslatedElement";
 import { LuCalendar } from "react-icons/lu";
 import formatDate from "utils/formatDate";
 
-interface BlogDetailsProps {
+interface BlogDetailsProps extends Partial<Multilingual & TranslatedElement> {
     title: string;
     description?: string;
     date?: Date;
+    slug?: string;
 }
 
-const BlogDetails = ({ title, description, date }: BlogDetailsProps) => (
-    <div flex="~ col" gap-sm>
-        <h1 text="primary 3xl" font-bold>
-            {title}
-        </h1>
+const BlogDetails = ({
+    title,
+    description,
+    date,
+    languages,
+    language,
+    slug,
+}: BlogDetailsProps) => {
+    console.log(languages);
 
-        {description && <p text="secondary xl">{description}</p>}
+    return (
+        <div flex="~ col" gap-sm>
+            <h1 text="primary 3xl" font-bold>
+                {title}
+            </h1>
 
-        {date && (
-            <time dateTime={formatDate(date)} text-secondary align-icon gap-2>
-                <LuCalendar role="presentation" focusable="false" />
-                {formatDate(date)}
-            </time>
-        )}
-    </div>
-);
+            {description && <p text="secondary xl">{description}</p>}
+
+            {date && (
+                <time
+                    dateTime={formatDate(date)}
+                    text-secondary
+                    align-icon
+                    gap-2
+                >
+                    <LuCalendar role="presentation" focusable="false" />
+                    {formatDate(date)}
+                </time>
+            )}
+
+            {languages && language && languages.length > 1 && (
+                <LanguageSelector
+                    defaultLanguage={language}
+                    languages={languages}
+                    slug={slug}
+                />
+            )}
+        </div>
+    );
+};
 
 export type { BlogDetailsProps };
 export default BlogDetails;
