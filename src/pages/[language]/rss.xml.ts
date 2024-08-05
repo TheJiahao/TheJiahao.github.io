2d7c3754a40719cbea07baf723ace2a15360c2f9
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-08-05 20:40:34 +0300

    fix(rss): 🐛 MDX not rendered correctly
    See: https://github.com/withastro/roadmap/discussions/419
    See: https://docs.astro.build/en/reference/container-reference

--- src/pages/[language]/rss.xml.ts
+++ src/pages/[language]/rss.xml.ts
@@ -1,24 +1,33 @@
+import reactRenderer from "@astrojs/react/server.js";
 import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import mdxRenderer from "astro/jsx/server.js";
 import { getAbsoluteLocaleUrl } from "astro:i18n";
 import { SITE_DESCRIPTION, SITE_TITLE } from "config";
 import { languageCodes } from "localization";
-import MarkdownIt from "markdown-it";
 import sanitizeHtml from "sanitize-html";
 import getBlogs from "utils/getBlogs";
+
+const container = await AstroContainer.create();
+container.addServerRenderer({ renderer: mdxRenderer, name: "mdx" });
+container.addServerRenderer({ renderer: reactRenderer, name: "react" });
+container.addClientRenderer({
+    name: "@astrojs/react",
+    entrypoint: "@astrojs/react/client.js",
+});
 
 export function getStaticPaths() {
     return languageCodes.map((language) => ({ params: { language } }));
 }
 
-export function GET({ params, site }: APIContext) {
+export async function GET({ params, site }: APIContext) {
     if (!site) {
         return new Response("Site is not defined", { status: 500 });
     }
 
     const language = params.language;
     const blogs = getBlogs();
-    const parser = new MarkdownIt();
 
     if (!language) {
         throw new TypeError("Invalid language");
@@ -28,15 +37,25 @@ export function GET({ params, site }: APIContext) {
         title: SITE_TITLE[language],
         description: SITE_DESCRIPTION[language],
         site: getAbsoluteLocaleUrl(language),
-        items: blogs.map(({ data, slug, body }) => ({
-            title: data.title,
-            description: data.description,
-            link: getAbsoluteLocaleUrl(language, `posts/${slug}`),
-            pubDate: data.date,
-            content: sanitizeHtml(parser.render(body), {
-                allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+        items: await Promise.all(
+            blogs.map(async ({ data, render, slug }) => {
+                const { title, description, date: pubDate } = data;
+                const { Content } = await render();
+                const postHTML = await container.renderToString(Content);
+
+                return {
+                    title,
+                    description,
+                    link: getAbsoluteLocaleUrl(language, `posts/${slug}`),
+                    pubDate,
+                    content: sanitizeHtml(postHTML, {
+                        allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+                            "img",
+                        ]),
+                    }),
+                };
             }),
-        })),
+        ),
         customData: `<language>${language}</language>`,
     });
 }
