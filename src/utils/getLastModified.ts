2ec3ef2e46d654a4bf83c4d3678f2b8803d47216
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-08-11 15:23:27 +0300

    docs(get-last-modified): 📚️ Add JSDoc

--- src/utils/getLastModified.ts
+++ src/utils/getLastModified.ts
@@ -1,5 +1,11 @@
 import { execSync } from "child_process";
 
+/**
+ * Gets last modified time from Git history.
+ *
+ * @param file File to get the last modified date
+ * @returns Last modified time
+ */
 export const getLastModified = (file: string): Date => {
     const result = execSync(
         `git log -1 --pretty="format:%cI" "${file}"`,
