ed1f5f6668ee1574c8ffd769d109248b5ed633f4
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-08-13 21:26:49 +0300

    fix(link-card): 🐛 Highlight when focused

--- src/components/molecules/LinkCard.tsx
+++ src/components/molecules/LinkCard.tsx
@@ -6,7 +6,7 @@ interface LinkProps extends Omit<Page, "description" | "image"> {
 }
 
 const LinkCard = ({ title, description, url, image }: LinkProps) => (
-    <article title={title} clickable p-8>
+    <article title={title} clickable focus-within:bg-hover focus:bg-hover p-8>
         <a href={url} grid="~ cols-[minmax(0,1fr)_auto] flow-col" gap-8>
             <div tabIndex={-1}>
                 <h1
