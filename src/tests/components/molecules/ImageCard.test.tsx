79391c2b04881a0ca6190b57cb61461c4f9a3ce7
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-08-18 15:56:23 +0300

    test(image-card): 🚨 Add more tests
    Refs: #241

--- src/tests/components/molecules/ImageCard.test.tsx
+++ src/tests/components/molecules/ImageCard.test.tsx
@@ -6,30 +6,59 @@ describe("<ImageCard/>", () => {
     const title = "How to write unit tests for Astro components?";
     const description =
         "This blog post will teach you how to write unit tests for Astro components.";
-    const date = new Date("2024-05-21");
     const url = "astro-unit-tests";
+    const date = "2024-05-01";
 
-    beforeEach(() => {
-        render(<ImageCard {...{ title, description, date, url }} />);
+    describe("with date", () => {
+        let time: HTMLElement;
+
+        beforeEach(() => {
+            render(<ImageCard title={title} url={url} date={new Date(date)} />);
+            time = screen.getByRole("time");
+        });
+
+        test("date is rendered", () => {
+            expect(time).toBeVisible();
+        });
+
+        test("date is correct", () => {
+            expect(time).toHaveAttribute("datetime", date);
+            expect(time).toHaveTextContent(date);
+        });
+
+        test("has title", () => {
+            expect(screen.getByRole("heading")).toHaveTextContent(title);
+        });
+
+        test("has link", () => {
+            expect(screen.getByRole("link")).toHaveAttribute("href");
+        });
     });
 
-    test("is rendered", () => {
-        expect(screen.getByRole("article")).toBeVisible();
+    describe("with description", () => {
+        beforeEach(() => {
+            render(
+                <ImageCard
+                    title={title}
+                    url={url}
+                    date={new Date(date)}
+                    description={description}
+                />,
+            );
+        });
+
+        test("has description", () => {
+            expect(screen.getByText(description)).toBeVisible();
+        });
     });
 
-    test("has title", () => {
-        expect(screen.getByRole("heading")).toHaveTextContent(title);
-    });
+    describe("without date", () => {
+        beforeEach(() => {
+            render(<ImageCard title={"Something"} url={url} />);
+        });
 
-    test("has description", () => {
-        expect(screen.getByText(description)).toBeVisible();
-    });
-
-    test("has date", () => {
-        expect(screen.getByText("2024-05-21")).toBeVisible();
-    });
-
-    test("has link", () => {
-        expect(screen.getByRole("link")).toHaveAttribute("href");
+        test("date is not rendered", () => {
+            expect(screen.queryByRole("time")).not.toBeInTheDocument();
+        });
     });
 });
