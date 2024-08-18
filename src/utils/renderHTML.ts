ffacabd7ed95374516cc4737272a5b6363468da4
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-08-18 14:38:34 +0300

    refactor: ♻️ Extract Markdown rendering

--- src/utils/renderHTML.ts
+++ src/utils/renderHTML.ts
@@ -2,7 +2,10 @@ import reactRenderer from "@astrojs/react/server.js";
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import mdxRenderer from "astro/jsx/server.js";
 import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
 
+const markdownParset = new MarkdownIt();
 const container = await AstroContainer.create();
 
 container.addServerRenderer({ renderer: mdxRenderer, name: "mdx" });
@@ -20,3 +23,13 @@ container.addClientRenderer({
  */
 export const renderHTML = async (component: AstroComponentFactory) =>
     await container.renderToString(component);
+
+/**
+ * Renders Markdown to HTML.
+ *
+ * @param content Markdown string to render
+ * @returns HTML string
+ */
+export const renderMarkdown = (content: string): string => {
+    return sanitizeHtml(markdownParset.renderInline(content));
+};
