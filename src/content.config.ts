ec24aaf48bf08db85232192e784bef287ea915df
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-12-04 18:30:32 +0200

    refactor(schema): ♻️ Extract base schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,39 +1,42 @@
 import { glob } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z, type SchemaContext } from "astro:content";
+
+const baseSchema = ({ image }: SchemaContext) =>
+    z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        image: image().optional(),
+    });
 
 const postCollection = defineCollection({
     loader: glob({ pattern: "**/*.{md,mdx}", base: "content/posts" }),
-    schema: ({ image }) =>
-        z.object({
-            title: z.string(),
+    schema: (context) => {
+        baseSchema(context).extend({
             date: z.date(),
             license: z.string().optional(),
             tags: z.array(z.string()).optional(),
-            description: z.string().optional(),
-            image: image().optional(),
             links: z
                 .array(
-                    z.object({
-                        title: z.string(),
-                        description: z.string().optional(),
+                    baseSchema(context).extend({
                         url: z.string(),
-                        image: z.union([image(), z.string()]).optional(),
+                        image: z
+                            .union([context.image(), z.string()])
+                            .optional(),
                     }),
                 )
                 .optional(),
             draft: z.boolean().optional(),
             math: z.boolean().optional(),
-        }),
+        });
+    },
 });
 
 const projectCollection = defineCollection({
     loader: glob({ pattern: "**/*.json", base: "content/projects" }),
-    schema: ({ image }) =>
-        z.object({
-            title: z.string(),
+    schema: (context) =>
+        baseSchema(context).extend({
             url: z.string(),
             description: z.record(z.string(), z.string()),
-            image: image().optional(),
         }),
 });
 
