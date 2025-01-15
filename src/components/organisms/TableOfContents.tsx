a76f1a9d9d79f791d58beefaf71de62b9eca44db
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-01-15 17:50:26 +0200

    refactor: ♻️ Remove unnecessary indent level

--- src/components/organisms/TableOfContents.tsx
+++ src/components/organisms/TableOfContents.tsx
@@ -6,19 +6,14 @@ interface TableOfContentsProps {
     headings: MarkdownHeading[];
 }
 
-const TableOfContents = ({ headings }: TableOfContentsProps) => {
-    return (
-        <nav card max-w-30ch p-lg text-secondary>
-            <ul>
-                {getTOC(headings).map((heading) => (
-                    <TableOfContentsHeading
-                        key={heading.slug}
-                        heading={heading}
-                    />
-                ))}
-            </ul>
-        </nav>
-    );
-};
+const TableOfContents = ({ headings }: TableOfContentsProps) => (
+    <nav card max-w-30ch p-lg text-secondary>
+        <ul>
+            {getTOC(headings).map((heading) => (
+                <TableOfContentsHeading key={heading.slug} heading={heading} />
+            ))}
+        </ul>
+    </nav>
+);
 
 export default TableOfContents;
