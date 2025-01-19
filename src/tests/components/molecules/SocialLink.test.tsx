9f57f7183caaf20b49897469528b4b8cb1c0367b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-01-19 21:33:12 +0200

    test: 🚨 Disable unwanted ESlint rule

--- src/tests/components/molecules/SocialLink.test.tsx
+++ src/tests/components/molecules/SocialLink.test.tsx
@@ -3,22 +3,23 @@ import SocialLink from "components/molecules/SocialLink";
 import { LuGithub, LuLinkedin, LuRss, LuSquare } from "react-icons/lu";
 import { describe, expect, test } from "vitest";
 
-describe("<SocialLink/>", () => {
-    test.each([
-        <LuRss key={0} />,
-        <LuGithub key={0} />,
-        <LuLinkedin key={0} />,
-    ])("renders correct icon %s", (icon) => {
-        render(
-            <SocialLink
-                url="https://example.com"
-                icon={icon}
-                label="Social link"
-            />,
-        );
+/* eslint-disable react/jsx-key */
 
-        expect(screen.getByRole("presentation")).toBeInTheDocument();
-    });
+describe("<SocialLink/>", () => {
+    test.each([<LuRss />, <LuGithub />, <LuLinkedin />])(
+        "renders correct icon %s",
+        (icon) => {
+            render(
+                <SocialLink
+                    url="https://example.com"
+                    icon={icon}
+                    label="Social link"
+                />,
+            );
+
+            expect(screen.getByRole("presentation")).toBeInTheDocument();
+        },
+    );
 
     test.each(["/rss.xml", "https://www.mysocialpage.com", "/about"])(
         "has correct url %s",
