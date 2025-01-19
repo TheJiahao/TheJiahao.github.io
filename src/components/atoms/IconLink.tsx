ecbb1270a71b4e4b7f8dbf380de8a4b14bf4eb6d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-01-19 20:04:37 +0200

    feat(icon-link): ✨ Add only-icon mode

--- src/components/atoms/IconLink.tsx
+++ src/components/atoms/IconLink.tsx
@@ -1,16 +1,49 @@
 import type { ReactElement } from "react";
+import type { XOR } from "ts-essentials";
 
-interface LabeledIconLinkProps {
+interface Labeled {
+    label: string;
+}
+
+interface NotLabeled {
+    title: string;
+}
+
+type LabeledIconLinkProps = XOR<Labeled, NotLabeled> & {
     url: string;
     label: string;
     icon: ReactElement;
-}
+    rel?: string;
+};
 
-const IconLink = ({ label, url, icon }: LabeledIconLinkProps) => (
-    <a href={url} text="secondary lg" clickable card align-icon p-2 gap-2>
-        {icon}
-        {label}
-    </a>
-);
+const IconLink = ({ label, title, url, icon, rel }: LabeledIconLinkProps) =>
+    label ? (
+        <a
+            href={url}
+            rel={rel}
+            text="secondary lg"
+            clickable
+            card
+            align-icon
+            p-2
+            gap-2
+        >
+            {icon}
+            {label}
+        </a>
+    ) : (
+        <a
+            href={url}
+            aria-label={title}
+            title={title}
+            rel={rel}
+            block
+            clickable
+            rounded-md
+            p-1
+        >
+            {icon}
+        </a>
+    );
 
 export default IconLink;
