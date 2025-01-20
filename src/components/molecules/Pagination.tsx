5b6b3aa568f69823f9ae7c68b042802eb3de7aae
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-01-20 16:28:21 +0200

    feat(pagination): ✨ Add initial pagination
    Refs: #560

---
+++ src/components/molecules/Pagination.tsx
@@ -0,0 +1,38 @@
+import { getRelativeLocaleUrl } from "astro:i18n";
+import IconLink from "components/atoms/IconLink";
+import type { TranslatedElement } from "interfaces/TranslatedElement";
+import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
+import { getTranslation } from "utils/getTranslation";
+
+interface PaginationProps extends TranslatedElement {
+    currentPage: number;
+    previousUrl?: string;
+    nextUrl?: string;
+    defaultUrl?: string;
+}
+
+const Pagination = ({
+    currentPage,
+    previousUrl,
+    nextUrl,
+    language,
+    defaultUrl = getRelativeLocaleUrl(language),
+}: PaginationProps) => (
+    <nav>
+        <IconLink
+            url={previousUrl || defaultUrl}
+            label={getTranslation(language).previousPage}
+            icon={<LuChevronLeft />}
+            disabled={!previousUrl}
+        />
+        {currentPage}
+        <IconLink
+            url={nextUrl || defaultUrl}
+            label={getTranslation(language).nextPage}
+            icon={<LuChevronRight />}
+            disabled={!nextUrl}
+        />
+    </nav>
+);
+
+export default Pagination;
