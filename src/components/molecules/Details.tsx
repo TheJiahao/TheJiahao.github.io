7eb6402e466d27aeebd46f51ead96d77b61f0c42
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-01-24 22:01:27 +0200

    feat(transition): ✨ Add transition to blog title

--- src/components/molecules/Details.tsx
+++ src/components/molecules/Details.tsx
@@ -1,13 +1,19 @@
 import parse from "html-react-parser";
+import type { HTMLAttributes } from "react";
+import { getUUID } from "utils/getUUID";
 import { renderMarkdown } from "utils/renderMarkdown";
-interface DetailsProps {
+interface DetailsProps extends HTMLAttributes<HTMLDivElement> {
     title: string;
     description?: string;
 }
 
 const Details = ({ title, description }: DetailsProps) => (
     <div text="secondary xl" flex="~ col" gap-sm>
-        <h1 text="primary 3xl" font-bold>
+        <h1
+            text="primary 3xl"
+            font-bold
+            style={{ viewTransitionName: `title-${getUUID(title)}` }}
+        >
             {title}
         </h1>
 
