9b1d27c09bc99b19fce8388f4ca8d8eb07e5004f
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-01-25 09:26:46 +0200

    fix: 🐛 Prevent TOC shrinking

--- src/components/organisms/TableOfContents.tsx
+++ src/components/organisms/TableOfContents.tsx
@@ -7,7 +7,7 @@ interface TableOfContentsProps {
 }
 
 const TableOfContents = ({ headings }: TableOfContentsProps) => (
-    <nav card max-w-30ch p-lg text-secondary>
+    <nav card w-30ch p-lg text-secondary>
         <ul>
             {getTOC(headings).map((heading) => (
                 <TableOfContentsHeading key={heading.slug} heading={heading} />
